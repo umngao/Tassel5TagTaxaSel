25b71b4146c390ff7624b1b213bbb1649b7075d9
Peter Bradbury <pjb39@cornell.edu>
2014-05-22 15:45:55 -0400

    implementing methods in the phenotype package

--- src/net/maizegenetics/phenotype/FilterPhenotype.java
+++ src/net/maizegenetics/phenotype/FilterPhenotype.java
@@ -3,59 +3,39 @@ package net.maizegenetics.phenotype;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
-
-import com.google.common.collect.ArrayListMultimap;
+import java.util.TreeSet;
 
 import net.maizegenetics.taxa.TaxaList;
 import net.maizegenetics.taxa.TaxaListBuilder;
 import net.maizegenetics.taxa.Taxon;
-import net.maizegenetics.util.TableReport;
 
-public class FilterPhenotype implements Phenotype, TableReport {
+
+public class FilterPhenotype extends AbstractPhenotype {
 	private final boolean myIsRowFilter;
 	private final int[] myRowRedirect;
-	private final ArrayList<PhenotypeAttribute> myAttributes; 
-	private final ArrayList<ATTRIBUTE_TYPE> myAttributeTypes;
-	private final ArrayListMultimap<ATTRIBUTE_TYPE, PhenotypeAttribute> attributeMultimap;
-	private final TaxaAttribute myTaxa;
-	private final int numberOfAttributes;
 	private final int numberOfObservations;
-	
-	private final String myTableTitle = "Filtered phenotype";
 	
 	//private constructors
 	
-	private FilterPhenotype(CorePhenotype basePheno, ArrayList<PhenotypeAttribute> retainedAttributes, TaxaList taxaToRetain) {
-		if (retainedAttributes == null) {
-			myAttributes = basePheno.getAttributeList();
-		} else {
-			myAttributes = new ArrayList<PhenotypeAttribute>(retainedAttributes);
-		}
+	private FilterPhenotype(AbstractPhenotype basePheno, String tablename, ArrayList<PhenotypeAttribute> retainedAttributes, ArrayList<ATTRIBUTE_TYPE> types, TaxaList taxaToRetain) {
+		super(tablename, retainedAttributes, types);
 		
-		numberOfAttributes = myAttributes.size();
-		myAttributeTypes = new ArrayList<>();
-		attributeMultimap = ArrayListMultimap.create();
-		for (PhenotypeAttribute attr : myAttributes) {
-			ATTRIBUTE_TYPE myType = basePheno.getAttributeType(attr);
-			myAttributeTypes.add(myType);
-			attributeMultimap.put(myType, attr);
-		}
-		
-		List<PhenotypeAttribute> taxaAttrList = attributeMultimap.get(ATTRIBUTE_TYPE.taxa);
-		if (taxaAttrList.size() != 1) myTaxa = null;
-		else myTaxa = (TaxaAttribute) taxaAttrList.get(0);
-
 		if (taxaToRetain == null) {
 			myIsRowFilter = false;
 			myRowRedirect = null;
 			numberOfObservations = basePheno.getNumberOfObservations();
+		} else if (taxaAttribute == null) {
+			myLogger.error(String.format("Error in FilterPhenotype constructor: %s has no taxa", tablename));
+			myIsRowFilter = false;
+			myRowRedirect = null;
+			numberOfObservations = basePheno.getNumberOfObservations();
 		} else {
+			
 			myIsRowFilter = true;
 			ArrayList<Taxon> retainedList = new ArrayList<>(taxaToRetain);
 			Collections.sort(retainedList);
 			int newObsNumber = 0;
-			Taxon[] originalTaxa = (Taxon[]) myTaxa.getValues();
+			Taxon[] originalTaxa = (Taxon[]) taxaAttribute.getValues();
 			int n = originalTaxa.length;
 			int[] newObs = new int[n];
 			for (int i = 0; i < n; i++) {
@@ -65,40 +45,26 @@ public class FilterPhenotype implements Phenotype, TableReport {
 			numberOfObservations = newObsNumber;
 		}
 		
-		
 	}
 	
-	private FilterPhenotype(FilterPhenotype basePheno, ArrayList<PhenotypeAttribute> retainedAttributes, TaxaList taxaToRetain) {
-		//TODO optimize for FilterPhenotype
-		if (retainedAttributes == null) {
-			myAttributes = basePheno.getAttributeList();
-		} else {
-			myAttributes = new ArrayList<PhenotypeAttribute>(retainedAttributes);
-		}
-		
-		numberOfAttributes = myAttributes.size();
-		myAttributeTypes = new ArrayList<>();
-		attributeMultimap = ArrayListMultimap.create();
-		for (PhenotypeAttribute attr : myAttributes) {
-			ATTRIBUTE_TYPE myType = basePheno.getAttributeType(attr);
-			myAttributeTypes.add(myType);
-			attributeMultimap.put(myType, attr);
-		}
-		
-		List<PhenotypeAttribute> taxaAttrList = attributeMultimap.get(ATTRIBUTE_TYPE.taxa);
-		if (taxaAttrList.size() != 1) myTaxa = null;
-		else myTaxa = (TaxaAttribute) taxaAttrList.get(0);
+	private FilterPhenotype(FilterPhenotype basePheno, String tablename, ArrayList<PhenotypeAttribute> retainedAttributes, ArrayList<ATTRIBUTE_TYPE> types, TaxaList taxaToRetain) {
+		super(tablename, retainedAttributes, types);
 
 		if (taxaToRetain == null) {
-			myIsRowFilter = false;
-			myRowRedirect = null;
+			myIsRowFilter = basePheno.myIsRowFilter;
+			myRowRedirect = basePheno.myRowRedirect;
+			numberOfObservations = basePheno.getNumberOfObservations();
+		} else if (taxaAttribute == null) {
+			myLogger.error(String.format("Error in FilterPhenotype constructor: %s has no taxa", tablename));
+			myIsRowFilter = basePheno.myIsRowFilter;
+			myRowRedirect = basePheno.myRowRedirect;
 			numberOfObservations = basePheno.getNumberOfObservations();
 		} else {
 			myIsRowFilter = true;
 			ArrayList<Taxon> retainedList = new ArrayList<>(taxaToRetain);
 			Collections.sort(retainedList);
 			int newObsNumber = 0;
-			Taxon[] originalTaxa = (Taxon[]) myTaxa.getValues();
+			Taxon[] originalTaxa = (Taxon[]) taxaAttribute.getValues();
 			int n = originalTaxa.length;
 			int[] newObs = new int[n];
 			for (int i = 0; i < n; i++) {
@@ -112,136 +78,88 @@ public class FilterPhenotype implements Phenotype, TableReport {
 	
 	//public static getInstance methods
 	
-	public static Phenotype getInstance(Phenotype basePheno, ArrayList<PhenotypeAttribute> retainedAttributes, TaxaList taxaToRetain) {
-		if (basePheno instanceof CorePhenotype) return new FilterPhenotype((CorePhenotype) basePheno, retainedAttributes, taxaToRetain);
-		else if (basePheno instanceof FilterPhenotype) return new FilterPhenotype((FilterPhenotype) basePheno, retainedAttributes, taxaToRetain);
+	public static Phenotype getInstance(Phenotype basePheno, ArrayList<PhenotypeAttribute> retainedAttributes, TaxaList taxaToRetain, String tablename) {
+		ArrayList<ATTRIBUTE_TYPE> types = new ArrayList<>();
+		for (PhenotypeAttribute attr : retainedAttributes) types.add(basePheno.getAttributeType(attr));
+		if (tablename == null || tablename.trim().length() == 0) tablename = "filtered_" + basePheno.getName();
+
+		if (basePheno instanceof CorePhenotype) return new FilterPhenotype((CorePhenotype) basePheno, tablename, retainedAttributes, types, taxaToRetain);
+		else if (basePheno instanceof FilterPhenotype) return new FilterPhenotype((FilterPhenotype) basePheno, tablename, retainedAttributes, types, taxaToRetain);
 		else return null;
 	}
 	
-	public static Phenotype getInstance(Phenotype basePheno, ArrayList<PhenotypeAttribute> retainedAttributes) {
-		if (basePheno instanceof CorePhenotype) return new FilterPhenotype((CorePhenotype) basePheno, retainedAttributes, null);
-		else if (basePheno instanceof FilterPhenotype) return new FilterPhenotype((FilterPhenotype) basePheno, retainedAttributes, null);
+	public static Phenotype getInstance(Phenotype basePheno, ArrayList<PhenotypeAttribute> retainedAttributes, String tablename) {
+		ArrayList<ATTRIBUTE_TYPE> types = new ArrayList<>();
+		for (PhenotypeAttribute attr : retainedAttributes) types.add(basePheno.getAttributeType(attr));
+		if (tablename == null || tablename.trim().length() == 0) tablename = "filtered_" + basePheno.getName();
+		
+		if (basePheno instanceof CorePhenotype) return new FilterPhenotype((CorePhenotype) basePheno, tablename, retainedAttributes, types, null);
+		else if (basePheno instanceof FilterPhenotype) return new FilterPhenotype((FilterPhenotype) basePheno, tablename, retainedAttributes, types, null);
 		else return null;
 	}
 	
-	public static Phenotype getInstance(Phenotype basePheno, TaxaList retainedTaxa) {
-		if (basePheno instanceof CorePhenotype) return new FilterPhenotype((CorePhenotype) basePheno, null, retainedTaxa);
-		else if (basePheno instanceof FilterPhenotype) return new FilterPhenotype((FilterPhenotype) basePheno, null, retainedTaxa);
+	public static Phenotype getInstance(Phenotype basePheno, TaxaList retainedTaxa, String tablename) {
+		if (tablename == null || tablename.trim().length() == 0) tablename = "filtered_" + basePheno.getName();
+		
+		if (basePheno instanceof CorePhenotype) {
+			return new FilterPhenotype((CorePhenotype) basePheno, tablename, basePheno.getAttributeList(), basePheno.getAttributeTypeList(), retainedTaxa);
+		}
+		else if (basePheno instanceof FilterPhenotype) {
+			return new FilterPhenotype((FilterPhenotype) basePheno, tablename, basePheno.getAttributeList(), basePheno.getAttributeTypeList(), retainedTaxa);
+		}
 		else return null;
 	}
 	
-	public static Phenotype getInstanceRemoveTaxa(Phenotype basePheno, TaxaList taxaToRemove) {
+	public static Phenotype getInstanceRemoveTaxa(Phenotype basePheno, TaxaList taxaToRemove, String tablename) {
 		ArrayList<Taxon> keepList = new ArrayList<Taxon>(basePheno.taxa());
 		keepList.removeAll(taxaToRemove);
 		TaxaListBuilder taxaBuilder = new TaxaListBuilder();
 		TaxaList keepTaxa = taxaBuilder.addAll(keepList).build();
+		if (tablename == null || tablename.trim().length() == 0) tablename = "filtered_" + basePheno.getName();
 		
-		if (basePheno instanceof CorePhenotype) return new FilterPhenotype((CorePhenotype) basePheno, null, keepTaxa);
-		else if (basePheno instanceof FilterPhenotype) return new FilterPhenotype((FilterPhenotype) basePheno, null, keepTaxa);
+		if (basePheno instanceof CorePhenotype) {
+			return new FilterPhenotype((CorePhenotype) basePheno, tablename, basePheno.getAttributeList(), basePheno.getAttributeTypeList(), keepTaxa);
+		}
+		else if (basePheno instanceof FilterPhenotype) {
+			return new FilterPhenotype((FilterPhenotype) basePheno, tablename, basePheno.getAttributeList(), basePheno.getAttributeTypeList(), keepTaxa);
+		}
 		else return null;
 	}
 	
-
-	@Override
-	public int getAttributeIndex(PhenotypeAttribute attr) {
-		// TODO Auto-generated method stub
-		return 0;
+	//public methods
+	
+	public int[] getRowRedirect() {
+		return myRowRedirect;
 	}
-
-	@Override
-	public ATTRIBUTE_TYPE getAttributeType(int attrnum) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ATTRIBUTE_TYPE getAttributeType(PhenotypeAttribute attribute) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setAttributeType(int attrnum, ATTRIBUTE_TYPE type) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setAttributeType(PhenotypeAttribute attribute,
-			ATTRIBUTE_TYPE type) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	//methods to override ------------------------------------
-	@Override
-	public Object getValue(int obs, int attrnum) {
-		if (myIsRowFilter) {
-			return myAttributes.get(attrnum).getValue(myRowRedirect[obs]);
-		} else {
-			return myAttributes.get(attrnum).getValue(obs);
-		}
-	}
-
-	@Override
-	public PhenotypeAttribute getAttribute(int attrnum) {
-		if (myIsRowFilter) {
-			return myAttributes.get(attrnum).getSubset(myRowRedirect);	
-		} else return myAttributes.get(attrnum);
-	}
-
-	@Override
-	public ArrayList<PhenotypeAttribute> getAttributeList() {
-		return new ArrayList<PhenotypeAttribute>(myAttributes);
-	}
-
-	@Override
-	public PhenotypeAttribute getAttributeOfType(ATTRIBUTE_TYPE type, int attrnum) {
-		return attributeMultimap.get(type).get(attrnum);
-	}
-
-	@Override
-	public List<PhenotypeAttribute> getAttributeListOfType(ATTRIBUTE_TYPE type) {
-		return new ArrayList<PhenotypeAttribute>(attributeMultimap.get(type));
-	}
-
-	@Override
-	public TaxaList taxa() {
-		if (myTaxa == null) return null;
-		
-		TaxaListBuilder taxaBuilder = new TaxaListBuilder();
-		Taxon[] taxa;
-		if (myIsRowFilter) {
-			taxa = (Taxon[]) myTaxa.getSubsetOfValues(myRowRedirect);
-		} else {
-			taxa = (Taxon[]) myTaxa.getValues();
-		}
-		
-		return taxaBuilder.addAll(taxa).build();
-	}
-
-	@Override
-	public int getNumberOfAttributes() {
-		return numberOfAttributes;
-	}
-
-	@Override
-	public int getNumberOfAttributesOfType(ATTRIBUTE_TYPE type) {
-		return attributeMultimap.get(type).size(); 
-	}
-
+	
 	@Override
 	public int getNumberOfObservations() {
 		return numberOfObservations;
 	}
 
 	@Override
-	public Object[] getTableColumnNames() {
-		String[] names = new String[numberOfAttributes];
-		for (int a = 0; a < numberOfAttributes; a++) {
-			names[a] = myAttributes.get(a).getName();
+	public Object getValue(int obs, int attrnum) {
+		return super.getValue(myRowRedirect[obs], attrnum);
+	}
+
+	@Override
+	public Object getValues(int attrnum) {
+		return getAttribute(attrnum).getSubsetOfValues(myRowRedirect);
+	}
+
+	@Override
+	public Object getValues(PhenotypeAttribute attr) {
+		return attr.getSubsetOfValues(myRowRedirect);
+	}
+
+	@Override
+	public TaxaList taxa() {
+		TreeSet<Taxon> taxonset = new TreeSet<>();
+		for (int ndx : myRowRedirect) {
+			taxonset.add((Taxon) taxaAttribute.getValue(ndx));
 		}
-		return names;
+		TaxaListBuilder taxaBuilder = new TaxaListBuilder();
+		return taxaBuilder.addAll(taxonset).build();
 	}
 
 	@Override
@@ -258,13 +176,13 @@ public class FilterPhenotype implements Phenotype, TableReport {
 	}
 
 	@Override
-	public String getTableTitle() {
-		return myTableTitle;
+	public Object[] getRow(int row) {
+		return super.getRow(myRowRedirect[row]);
 	}
 
 	@Override
-	public int getColumnCount() {
-		return numberOfAttributes;
+	public Object getValueAt(int row, int col) {
+		return super.getValueAt(myRowRedirect[row], col);
 	}
 
 	@Override
@@ -272,37 +190,5 @@ public class FilterPhenotype implements Phenotype, TableReport {
 		return numberOfObservations;
 	}
 
-	@Override
-	public Object[] getRow(int row) {
-		Object[] values = new Object[numberOfAttributes];
-		int obs;
-		if (myIsRowFilter) obs = myRowRedirect[row];
-		else obs = row;
-		for (int i = 0; i < numberOfAttributes; i++) {
-			values[i] = myAttributes.get(i).getValue(obs);
-		}
-		return values;
-	}
-
-	@Override
-	public Object[][] getTableData(int start, int end) {
-		int nrows = end - start + 1;
-		Object[][] resultTable = new Object[nrows][];
-		for (int r = start; r <= end; r++) resultTable[r] = getRow(r);
-		return resultTable;
-	}
-
-	@Override
-	public Object getValueAt(int row, int col) {
-		int obs;
-		if (myIsRowFilter) obs = myRowRedirect[row];
-		else obs = row;
-		return myAttributes.get(col).getValue(obs);
-	}
-
-	@Override
-	public int getElementCount() {
-		return getRowCount() * getColumnCount();
-	}
 
 }
--- src/net/maizegenetics/phenotype/Phenotype.java
+++ src/net/maizegenetics/phenotype/Phenotype.java
@@ -5,19 +5,59 @@ import java.util.List;
 
 import net.maizegenetics.taxa.TaxaList;
 
+/**
+ * Phenotype represents phenotype data as a two dimensional matrix. Rows are observations. Columns are attributes.
+ * 
+ * @author pbradbury
+ *
+ */
 public interface Phenotype {
 	public enum ATTRIBUTE_TYPE {data, covariate, factor, taxa};
 
+	/**
+	 * @param obs	an observation number
+	 * @param attrnum	the index or column number of the attribute
+	 * @return	the attribute value for the observation
+	 */
 	public abstract Object getValue(int obs, int attrnum);
+	
+	/**
+	 * @param attrnum	the index or column number of the attribute
+	 * @return	a 1 dimensional array of values for this attribute in observation order
+	 * The type of array returned is a function of the attribute class. The array will be a primitive type where feasible.
+	 * A CategoricalAttribute returns an array of int. A NumericAttribute returns an array of float. A TaxaAttribute returns an array of Taxon.
+	 */
+	public abstract Object getValues(int attrnum);
+	
+	/**
+	 * @param attr	an attribute
+	 * @return	a 1 dimensional array of values for this attribute in observation order
+	 * The type of array returned is a function of the attribute class. The array will be a primitive type where feasible.
+	 * A CategoricalAttribute returns an array of int. A NumericAttribute returns an array of float. A TaxaAttribute returns an array of Taxon.
+	 */
+	public abstract Object getValues(PhenotypeAttribute attr);
 
+	/**
+	 * @param attrnum	the index or column number of the attribute
+	 * @return	the attribute for column attrnum
+	 */
 	public abstract PhenotypeAttribute getAttribute(int attrnum);
 	
+	/**
+	 * @param attr	an attribute
+	 * @return	the index or column number for attribute attr
+	 */
 	public abstract int getAttributeIndex(PhenotypeAttribute attr);
 
 	/**
-	 * @return	a shallow copy of the attribute list. 
+	 * @return	a shallow copy of the attribute list 
 	 */
 	public abstract ArrayList<PhenotypeAttribute> getAttributeList();
+	
+	/**
+	 * @return a shallow copy of the attribute type list
+	 */
+	public abstract ArrayList<ATTRIBUTE_TYPE> getAttributeTypeList();
 
 	public abstract PhenotypeAttribute getAttributeOfType(ATTRIBUTE_TYPE type,
 			int attrnum);
@@ -40,5 +80,7 @@ public interface Phenotype {
 	public abstract void setAttributeType(PhenotypeAttribute attribute, ATTRIBUTE_TYPE type);
 
 	public abstract int getNumberOfObservations();
+	
+	public abstract String getName();
 
 }
