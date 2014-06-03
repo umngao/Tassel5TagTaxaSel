0b795b17ae356f1085cce6a4fbb46299c634c797
Terry Casstevens <tcasstevens@gmail.com>
2014-06-03 12:03:48 -0400

    Clean up allele depth classes.

--- src/net/maizegenetics/dna/snp/depth/AbstractAlleleDepth.java
+++ src/net/maizegenetics/dna/snp/depth/AbstractAlleleDepth.java
@@ -8,17 +8,17 @@ package net.maizegenetics.dna.snp.depth;
  * @author Terry Casstevens
  */
 public abstract class AbstractAlleleDepth implements AlleleDepth {
-    
+
     private final int myNumAlleles;
     private final int myNumTaxa;
     private final int myNumSites;
-    
+
     AbstractAlleleDepth(int numAlleles, int numTaxa, int numSites) {
         myNumAlleles = numAlleles;
         myNumTaxa = numTaxa;
         myNumSites = numSites;
     }
-    
+
     @Override
     public int[] depthForAlleles(int taxon, int site) {
         int[] result = new int[myNumAlleles];
@@ -27,12 +27,12 @@ public abstract class AbstractAlleleDepth implements AlleleDepth {
         }
         return result;
     }
-    
+
     @Override
     public int depthForAllele(int taxon, int site, int allele) {
         return AlleleDepthUtil.depthByteToInt(depthForAlleleByte(taxon, site, allele));
     }
-    
+
     @Override
     public int depth(int taxon, int site) {
         int result = 0;
@@ -41,20 +41,20 @@ public abstract class AbstractAlleleDepth implements AlleleDepth {
         }
         return result;
     }
-    
+
     @Override
     public int depthForTaxon(int taxon) {
         int result = 0;
-        for (int site=0; site<myNumSites; site++) {
+        for (int site = 0; site < myNumSites; site++) {
             result += depth(taxon, site);
         }
         return result;
     }
-    
+
     @Override
     public int depthForSite(int site) {
         int result = 0;
-        for (int taxon=0; taxon<myNumTaxa; taxon++) {
+        for (int taxon = 0; taxon < myNumTaxa; taxon++) {
             result += depth(taxon, site);
         }
         return result;
@@ -62,9 +62,9 @@ public abstract class AbstractAlleleDepth implements AlleleDepth {
 
     @Override
     public byte[][] depthAllSitesByte(int taxon) {
-        byte[][] result=new byte[myNumAlleles][myNumSites];
-        for (int allele=0; allele<myNumAlleles; allele++) {
-            for (int site=0; site<myNumSites; site++) {
+        byte[][] result = new byte[myNumAlleles][myNumSites];
+        for (int allele = 0; allele < myNumAlleles; allele++) {
+            for (int site = 0; site < myNumSites; site++) {
                 result[allele][site] += depthForAllele(taxon, site, allele);
             }
         }
