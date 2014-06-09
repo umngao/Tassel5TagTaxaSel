51ef3b1c81ffaeaf5217826cf106e30286d0c1eb
Terry Casstevens <tcasstevens@gmail.com>
2014-06-09 11:26:38 -0400

    TAS-398 Added utilities for setting logging level to DEBUG

--- src/net/maizegenetics/util/LoggingUtils.java
+++ src/net/maizegenetics/util/LoggingUtils.java
@@ -6,6 +6,7 @@ package net.maizegenetics.util;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
+import net.maizegenetics.prefs.TasselPrefs;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
@@ -26,25 +27,99 @@ public class LoggingUtils {
         // Utility Class
     }
 
+    /**
+     * Sends all logging messages (including log4j) to standard out. The logging
+     * level will be INFO, unless executed by the GUI and the user preference
+     * has been set level to DEBUG.
+     */
     public static void setupLogging() {
+        if (TasselPrefs.getLogDebug()) {
+            setupDebugLogging();
+        } else {
+            System.setOut(myOriginalOutputStream);
+            System.setErr(myOriginalErrStream);
+            sendLog4jToStdout();
+        }
+    }
+
+    /**
+     * Sends all logging messages (including log4j) to standard out. The logging
+     * level will be DEBUG.
+     */
+    public static void setupDebugLogging() {
         System.setOut(myOriginalOutputStream);
         System.setErr(myOriginalErrStream);
-        sendLog4jToStdout();
+        sendDebugLog4jToStdout();
     }
 
+    /**
+     * Sends all logging messages (including log4j) to specified PrintStream.
+     * The logging level will be INFO, unless executed by the GUI and the user
+     * preference has been set level to DEBUG. This is used to direct logging
+     * messages to the GUI.
+     *
+     * @param stream stream
+     */
     public static void setupLogging(PrintStream stream) {
+        if (TasselPrefs.getLogDebug()) {
+            setupDebugLogging(stream);
+        } else {
+            System.setOut(stream);
+            System.setErr(stream);
+            sendLog4jToStdout();
+        }
+    }
+
+    /**
+     * Sends all logging messages (including log4j) to specified PrintStream.
+     * The logging level will be DEBUG. This is used to direct logging messages
+     * to the GUI.
+     *
+     * @param stream
+     */
+    public static void setupDebugLogging(PrintStream stream) {
         System.setOut(stream);
         System.setErr(stream);
-        sendLog4jToStdout();
+        sendDebugLog4jToStdout();
     }
 
+    /**
+     * Sends all logging messages (including log4j) to specified file. The
+     * logging level will be INFO, unless executed by the GUI and the user
+     * preference has been set level to DEBUG.
+     *
+     * @param logFileName file name
+     *
+     * @throws FileNotFoundException
+     */
     public static void setupLogfile(String logFileName) throws FileNotFoundException {
+        if (TasselPrefs.getLogDebug()) {
+            setupDebugLogfile(logFileName);
+        } else {
+            File logFile = new File(logFileName);
+            myLogger.info("Log File: " + logFile.getAbsolutePath());
+            myPrintStream = new PrintStream(logFile);
+            System.setOut(myPrintStream);
+            System.setErr(myPrintStream);
+            sendLog4jToStdout();
+        }
+    }
+
+    /**
+     * Sends all logging messages (including log4j) to specified file. The
+     * logging level will be DEBUG.
+     *
+     * @param logFileName file name
+     *
+     * @throws FileNotFoundException
+     */
+    public static void setupDebugLogfile(String logFileName) throws FileNotFoundException {
         File logFile = new File(logFileName);
         myLogger.info("Log File: " + logFile.getAbsolutePath());
         myPrintStream = new PrintStream(logFile);
         System.setOut(myPrintStream);
         System.setErr(myPrintStream);
-        sendLog4jToStdout();
+        sendDebugLog4jToStdout();
     }
 
     public static void closeLogfile() {
@@ -58,6 +133,15 @@ public class LoggingUtils {
         props.setProperty("log4j.logger.net.maizegenetics", "INFO, stdout");
         props.setProperty("log4j.appender.stdout", "org.apache.log4j.ConsoleAppender");
         props.setProperty("log4j.appender.stdout.Threshold", "info");
+        props.setProperty("log4j.appender.stdout.layout", "org.apache.log4j.TTCCLayout");
+        PropertyConfigurator.configure(props);
+    }
+
+    private static void sendDebugLog4jToStdout() {
+        java.util.Properties props = new java.util.Properties();
+        props.setProperty("log4j.logger.net.maizegenetics", "DEBUG, stdout");
+        props.setProperty("log4j.appender.stdout", "org.apache.log4j.ConsoleAppender");
+        props.setProperty("log4j.appender.stdout.Threshold", "debug");
         props.setProperty("log4j.appender.stdout.layout", "org.apache.log4j.TTCCLayout");
         PropertyConfigurator.configure(props);
     }
