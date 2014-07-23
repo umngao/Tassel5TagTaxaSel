33f66777b492650860960ac0cfc54bacaf65fcbd
Terry Casstevens <tcasstevens@gmail.com>
2014-07-23 11:40:01 -0400

    TAS-446 Removing unused imports from GenomeFeatureMapBuilder

--- src/net/maizegenetics/dna/map/GenomeFeatureMapBuilder.java
+++ src/net/maizegenetics/dna/map/GenomeFeatureMapBuilder.java
@@ -10,7 +10,6 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import sun.org.mozilla.javascript.internal.json.JsonParser;
 
 import java.io.BufferedReader;
 import java.io.IOException;
