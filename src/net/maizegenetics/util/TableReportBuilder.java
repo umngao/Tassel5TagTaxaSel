bcd84728a82d4a2512587753bbecc4c63408b7f2
Terry Casstevens <tcasstevens@gmail.com>
2014-08-09 21:08:39 -0400

    TAS-492 Initial implementation of TableReportBuilder

---
+++ src/net/maizegenetics/util/TableReportBuilder.java
@@ -0,0 +1,108 @@
+/*
+ *  TableReportBuilder
+ * 
+ *  Created on Aug 9, 2014
+ */
+package net.maizegenetics.util;
+
+import com.google.common.base.Joiner;
+
+import java.io.BufferedWriter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author Terry Casstevens
+ */
+public class TableReportBuilder {
+
+    private static final Logger myLogger = Logger.getLogger(TableReportBuilder.class);
+
+    private static final String DELIMITER = "\t";
+
+    private final String myTableName;
+    private final Object[] myColumnNames;
+    private final List<Object[]> myData;
+    private final String myFilename;
+    private final BufferedWriter myWriter;
+    private final boolean myInMemory;
+    private final int myNumColumns;
+
+    private TableReportBuilder(String tableName, Object[] columnNames) {
+        myTableName = tableName;
+        myColumnNames = columnNames;
+        myNumColumns = columnNames.length;
+        myData = new ArrayList<>();
+        myFilename = null;
+        myWriter = null;
+        myInMemory = true;
+    }
+
+    private TableReportBuilder(String tableName, Object[] columnNames, String filename) {
+        myTableName = tableName;
+        myColumnNames = columnNames;
+        myNumColumns = columnNames.length;
+        myData = null;
+        myFilename = filename;
+        myWriter = Utils.getBufferedWriter(filename);
+        myInMemory = false;
+        try {
+            myWriter.write(Joiner.on(DELIMITER).join(myColumnNames));
+            myWriter.write("\n");
+        } catch (Exception e) {
+            myLogger.debug(e.getMessage(), e);
+            throw new IllegalStateException("TableReportBuilder: init: Problem adding headers to file: " + myFilename + ": " + e.getMessage());
+        }
+    }
+
+    public static TableReport readTableReport(String saveFile) {
+        return TableReportUtils.readDelimitedTableReport(saveFile, DELIMITER);
+    }
+
+    public static TableReportBuilder getInstance(String tableName, Object[] columnNames) {
+        return new TableReportBuilder(tableName, columnNames);
+    }
+
+    public static TableReportBuilder getInstance(String tableName, Object[] columnNames, String filename) {
+        return new TableReportBuilder(tableName, columnNames, filename);
+    }
+
+    public void add(Object[] row) {
+
+        if (myNumColumns != row.length) {
+            throw new IllegalArgumentException("TableReportBuilder: add: number of row elements: " + row.length + " doesn't equal number of headers: " + myNumColumns);
+        }
+
+        if (myInMemory) {
+            myData.add(row);
+        } else {
+            try {
+                myWriter.write(Joiner.on(DELIMITER).join(row));
+                myWriter.write("\n");
+            } catch (Exception e) {
+                myLogger.debug(e.getMessage(), e);
+                throw new IllegalStateException("TableReportBuilder: add: Problem adding row to file: " + myFilename + ": " + e.getMessage());
+            }
+        }
+
+    }
+
+    public TableReport build() {
+        if (myInMemory) {
+            return new SimpleTableReport(myTableName, myColumnNames, myData.toArray(new Object[myData.size()][]));
+        } else {
+            try {
+                myWriter.close();
+            } catch (Exception e) {
+                myLogger.debug(e.getMessage(), e);
+                throw new IllegalStateException("TableReportBuilder: build: Problem closing file: " + myFilename + ": " + e.getMessage());
+            }
+            return null;
+        }
+    }
+
+}
