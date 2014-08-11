63b2f16f58124c5c326642bb838ccf29bc266650
fl262 <fl262@rrdhcp-10-32-115-123.redrover.cornell.edu>
2014-08-11 11:57:22 -0400

    Start code for sequence reads QC and contiging

---
+++ src/net/maizegenetics/dna/read/PERead.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.maizegenetics.dna.read;
+
+/**
+ *
+ * @author Fei Lu
+ */
+public class PERead {
+    public String readF = null;
+    public String readR = null;
+    public byte[] scoreF = null;
+    public byte[] scoreR = null;
+    public String pContig = null;
+    public byte[] pScore = null;
+    
+    public PERead (String readF, String readR, String scoreFS, String scoreFR) {
+        
+    }
+}
