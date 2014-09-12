3adba1404fd402849c72fec9dd06aa834dfcb59c
Terry Casstevens <tcasstevens@gmail.com>
2014-09-12 14:50:07 -0400

    Added code to run_pipeline.pl and start_tassel.pl to fix em-dashes when setting minimum and maximum heap size.

--- run_pipeline.pl
+++ run_pipeline.pl
@@ -29,9 +29,11 @@ my @args;
 for (my $i=0; $i<=$#ARGV; $i++){
    if ($ARGV[$i] =~ m/Xms/) {
       $java_mem_min .= "$ARGV[$i]";
+      $java_mem_min=~s/–/-/g;
    }
    elsif ($ARGV[$i] =~ m/Xmx/) {
       $java_mem_max .= "$ARGV[$i]";
+      $java_mem_max=~s/–/-/g;
    }
    else{
       push(@args, $ARGV[$i]);
