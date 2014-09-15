8e4f5fcf2c8e032c3bbfd4c71be89594dcf48aa7
pjbradbury <pjb39@cornell.edu>
2014-09-15 08:47:51 -0400

    TAS-511 Creates class to implement Matrix eQTL method for OLS solution to linear models

---
+++ src/net/maizegenetics/analysis/association/DiscreteSitesMatrixFELM.java
@@ -0,0 +1,131 @@
+package net.maizegenetics.analysis.association;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import net.maizegenetics.matrixalgebra.Matrix.DoubleMatrix;
+import net.maizegenetics.matrixalgebra.Matrix.DoubleMatrixFactory;
+import net.maizegenetics.plugindef.Datum;
+import net.maizegenetics.stats.linearmodels.ModelEffect;
+import net.maizegenetics.stats.linearmodels.ModelEffectUtils;
+import net.maizegenetics.util.TableReportBuilder;
+
+public class DiscreteSitesMatrixFELM extends AbstractFixedEffectLM {
+	protected int numberOfSiteReportColumns;
+	protected int numberOfAlleleReportColumns;
+	protected double tol = 1e-10;
+	protected ArrayList<DoubleMatrix> basisVectors;
+	protected ArrayList<double[]> basisArrays;
+	protected double[] normalizedData;
+	
+	public DiscreteSitesMatrixFELM(Datum dataset) {
+		super(dataset);
+	}
+
+	@Override
+	public void initializeReportBuilders() {
+		String tableName = "GLM Site Tests - " + myDatum.getName();
+		String[] columnNames;
+		if (permute) columnNames = new String[]{"Trait","Marker","Chr","Position","marker_F","marker_p","perm_p","marker_Rsq","marker_df","marker_MS","error_df","error_MS","model_df","model_MS" };
+		else columnNames = new String[]{"Trait","Marker","Chr","Position","marker_F","marker_p","marker_Rsq","marker_df","marker_MS","error_df","error_MS","model_df","model_MS" };
+		numberOfSiteReportColumns = columnNames.length;
+		siteReportBuilder = TableReportBuilder.getInstance(tableName, columnNames);
+		tableName = "GLM Allele Estimates - " + myDatum.getName();
+		columnNames = new String[]{"Trait","Marker","Chr","Position","Obs","Allele","Estimate"};
+		numberOfAlleleReportColumns = columnNames.length;
+		alleleReportBuilder = TableReportBuilder.getInstance(tableName, columnNames);
+		permpvalueColumn = 6;
+		markerpvalueColumn = 5;
+	}
+
+	@Override
+	protected void analyzeSite(int siteNumber, ArrayList<ModelEffect> baseModel) {
+		// TODO implement
+		byte[] geno = myGenoPheno.genotypeTable().alleles(siteNumber);
+		byte major = myGenoPheno.genotypeTable().majorAllele(siteNumber);
+		double[] coding = ModelEffectUtils.getNumericCodingForAdditiveModel(geno, major);
+		double[] normalGeno = normalizeVector(coding, true);
+		double corr = 0;
+		for (int i = 0; i < normalGeno.length; i++) corr += normalGeno[i] * normalizedData[i];
+		
+		//report
+	}
+
+	private void calculateBasis(List<ModelEffect> baseModel) {
+		//assume no missing data
+		//create othornormal basis for the base model
+		ArrayList<DoubleMatrix> modelVectors = new ArrayList<>();
+		ArrayList<ModelEffect> effects = new ArrayList<>(baseModel);
+		effects.remove(0); //do not use the mean
+		for (ModelEffect effect : effects) {
+			DoubleMatrix X = effect.getX();
+			int ncol = X.numberOfColumns();
+			int nrow = X.numberOfRows();
+			for (int i = 0; i < ncol; i++) {
+				DoubleMatrix aColumn = X.column(i);
+				//center
+				double mean = X.columnSum(i) / nrow;
+				aColumn = aColumn.scalarAdd(-mean);
+				modelVectors.add(aColumn);
+			}
+			//for each vector v, subtract <u,v>*u for each of the previously calculated unit vectors
+			basisVectors = new ArrayList<>();
+			for (DoubleMatrix vector:modelVectors) {
+				DoubleMatrix basis = vector.copy();
+				for (DoubleMatrix u:basisVectors) {
+					double alpha = u.crossproduct(vector).get(0, 0);
+					basis.minusEquals(u.scalarMult(alpha));
+				}
+				double norm = Math.sqrt(basis.crossproduct(basis).get(0, 0));
+				//if norm = 0, the vector is linearly dependent on previous vectors and is not part of the basis
+				if (norm > tol) {
+					basis.scalarMultEquals(1/norm);
+					basisVectors.add(basis);
+				}
+			}
+		}
+		
+		basisArrays = new ArrayList<>();
+		int n = basisVectors.get(0).numberOfRows();
+		for (DoubleMatrix dm : basisVectors) {
+			double[] vals = new double[n];
+			for (int i = 0; i < n; i++) vals[i] = dm.get(i, 0);
+			basisArrays.add(vals);
+		}
+		
+		normalizedData = normalizeVector(allData, true);
+	}
+	
+	private void normalizeData() {
+		
+	}
+	
+	private double[] normalizeVector(double[] input, boolean center) {
+		int n = input.length;
+		if (center) {
+			double mean = 0;
+			for (int i = 0; i < n; i++) mean += input[i];
+			mean /= (double) n;
+			double[] centered = new double[n];
+			for (int i = 0; i < n; i++) {
+				centered[i] = input[i] - mean;
+			}
+			input = centered;
+		} 
+		double[] output = Arrays.copyOf(input, n);
+		
+		for (double[] basis:basisArrays) {
+			double alpha = 0;
+			for (int i = 0; i < n; i++) alpha += input[i]*basis[i];
+			for (int i = 0; i < n; i++) output[i] -= alpha*basis[i];
+		}
+		
+		double norm = 0;
+		for (int i = 0; i < n; i++) norm += output[i] * output[i];
+		for (int i = 0; i < n; i++) output[i] /= norm;
+		
+		return output;
+	}
+}
