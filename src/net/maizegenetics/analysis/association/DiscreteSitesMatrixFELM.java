37c515c1bc60bf1043d3ae8adc8e23d60cdd0c91
Peter Bradbury <pjb39@cornell.edu>
2014-09-16 15:00:15 -0400

    TAS-511 fixed bugs in DiscreteSitesMatrixFELM, a test implementation of the method from Matrix eQTL.

--- src/net/maizegenetics/analysis/association/DiscreteSitesMatrixFELM.java
+++ src/net/maizegenetics/analysis/association/DiscreteSitesMatrixFELM.java
@@ -8,8 +8,11 @@ import java.util.List;
 import net.maizegenetics.matrixalgebra.Matrix.DoubleMatrix;
 import net.maizegenetics.matrixalgebra.Matrix.DoubleMatrixFactory;
 import net.maizegenetics.plugindef.Datum;
+import net.maizegenetics.stats.linearmodels.LinearModelUtils;
 import net.maizegenetics.stats.linearmodels.ModelEffect;
 import net.maizegenetics.stats.linearmodels.ModelEffectUtils;
+import net.maizegenetics.util.BitSet;
+import net.maizegenetics.util.OpenBitSet;
 import net.maizegenetics.util.TableReportBuilder;
 
 public class DiscreteSitesMatrixFELM extends AbstractFixedEffectLM {
@@ -19,6 +22,8 @@ public class DiscreteSitesMatrixFELM extends AbstractFixedEffectLM {
 	protected ArrayList<DoubleMatrix> basisVectors;
 	protected ArrayList<double[]> basisArrays;
 	protected double[] normalizedData;
+	protected int modeldf, errordf;
+	protected boolean hasBasis = false;
 	
 	public DiscreteSitesMatrixFELM(Datum dataset) {
 		super(dataset);
@@ -28,8 +33,8 @@ public class DiscreteSitesMatrixFELM extends AbstractFixedEffectLM {
 	public void initializeReportBuilders() {
 		String tableName = "GLM Site Tests - " + myDatum.getName();
 		String[] columnNames;
-		if (permute) columnNames = new String[]{"Trait","Marker","Chr","Position","marker_F","marker_p","perm_p","marker_Rsq","marker_df","marker_MS","error_df","error_MS","model_df","model_MS" };
-		else columnNames = new String[]{"Trait","Marker","Chr","Position","marker_F","marker_p","marker_Rsq","marker_df","marker_MS","error_df","error_MS","model_df","model_MS" };
+		if (permute) columnNames = new String[]{"Trait","Marker","Chr","Position","marker_F","marker_p","perm_p","marker_df","error_df","model_df"};
+		else columnNames = new String[]{"Trait","Marker","Chr","Position","marker_F","marker_p","marker_df","error_df","model_df"};
 		numberOfSiteReportColumns = columnNames.length;
 		siteReportBuilder = TableReportBuilder.getInstance(tableName, columnNames);
 		tableName = "GLM Allele Estimates - " + myDatum.getName();
@@ -41,23 +46,59 @@ public class DiscreteSitesMatrixFELM extends AbstractFixedEffectLM {
 	}
 
 	@Override
-	protected void analyzeSite(int siteNumber, ArrayList<ModelEffect> baseModel) {
-		// TODO implement
-		byte[] geno = myGenoPheno.genotypeTable().alleles(siteNumber);
-		byte major = myGenoPheno.genotypeTable().majorAllele(siteNumber);
-		double[] coding = ModelEffectUtils.getNumericCodingForAdditiveModel(geno, major);
+	protected void analyzeSite() {
+		if (!hasBasis) {
+			hasBasis = true;
+			calculateBasis();
+		}
+		byte[] geno = myGenoPheno.genotypeTable().genotypeAllTaxa(myCurrentSite);
+		byte minor = myGenoPheno.genotypeTable().minorAllele(myCurrentSite);
+		double[] coding = ModelEffectUtils.getNumericCodingForAdditiveModel(geno, minor);
 		double[] normalGeno = normalizeVector(coding, true);
-		double corr = 0;
-		for (int i = 0; i < normalGeno.length; i++) corr += normalGeno[i] * normalizedData[i];
+		double F,p;
+		int markerdf = 1;
+		if (normalGeno.length == 0) {	//genotype df = 0, do not test
+			F = 0;
+			p = Double.NaN;
+			markerdf = 0;
+		} else {
+			double corr = 0;
+			for (int i = 0; i < normalGeno.length; i++) corr += normalGeno[i] * normalizedData[i];
+			double corrsq = corr * corr;
+			F = errordf * corrsq / (1 - corrsq );
+	        try {
+	        	p = LinearModelUtils.Ftest(F, 1, errordf);
+	        } catch (Exception e) {
+	        	p = Double.NaN;
+	        }
+		}
+
 		
 		//report
+        //add results to site report
+        //columns = {"Trait","Marker","Chr","Position","marker_F","marker_p","perm_p","marker_df","error_df","model_df"}
+		String siteName = myGenoPheno.genotypeTable().siteName(myCurrentSite);
+        Object[] rowData = new Object[numberOfSiteReportColumns];
+        int columnCount = 0;
+        rowData[columnCount++] = currentTraitName;
+        rowData[columnCount++] = siteName;
+        rowData[columnCount++] = myGenoPheno.genotypeTable().chromosomeName(myCurrentSite);
+        rowData[columnCount++] = myGenoPheno.genotypeTable().chromosomalPosition(myCurrentSite);
+        rowData[columnCount++] = new Double(F);
+        rowData[columnCount++] = new Double(p);
+        if (permute) rowData[columnCount++] = "";
+        rowData[columnCount++] = new Integer(markerdf);
+        rowData[columnCount++] = new Integer(errordf);
+        rowData[columnCount++] = new Integer(modeldf - 1);
+        siteReportBuilder.add(rowData);
 	}
 
-	private void calculateBasis(List<ModelEffect> baseModel) {
+	public void calculateBasis() {
 		//assume no missing data
 		//create othornormal basis for the base model
+		
 		ArrayList<DoubleMatrix> modelVectors = new ArrayList<>();
-		ArrayList<ModelEffect> effects = new ArrayList<>(baseModel);
+		ArrayList<ModelEffect> effects = new ArrayList<>(myBaseModel);
 		effects.remove(0); //do not use the mean
 		for (ModelEffect effect : effects) {
 			DoubleMatrix X = effect.getX();
@@ -70,36 +111,35 @@ public class DiscreteSitesMatrixFELM extends AbstractFixedEffectLM {
 				aColumn = aColumn.scalarAdd(-mean);
 				modelVectors.add(aColumn);
 			}
-			//for each vector v, subtract <u,v>*u for each of the previously calculated unit vectors
-			basisVectors = new ArrayList<>();
-			for (DoubleMatrix vector:modelVectors) {
-				DoubleMatrix basis = vector.copy();
-				for (DoubleMatrix u:basisVectors) {
-					double alpha = u.crossproduct(vector).get(0, 0);
-					basis.minusEquals(u.scalarMult(alpha));
-				}
-				double norm = Math.sqrt(basis.crossproduct(basis).get(0, 0));
-				//if norm = 0, the vector is linearly dependent on previous vectors and is not part of the basis
-				if (norm > tol) {
-					basis.scalarMultEquals(1/norm);
-					basisVectors.add(basis);
-				}
-			}
 		}
 		
+		//for each vector v, subtract <u,v>*u for each of the previously calculated unit vectors
+		basisVectors = new ArrayList<>();
+		for (DoubleMatrix vector:modelVectors) {
+			DoubleMatrix basis = vector.copy();
+			for (DoubleMatrix u:basisVectors) {
+				double alpha = u.crossproduct(vector).get(0, 0);
+				basis.minusEquals(u.scalarMult(alpha));
+			}
+			double norm = Math.sqrt(basis.crossproduct(basis).get(0, 0));
+			//if norm = 0, the vector is linearly dependent on previous vectors and is not part of the basis
+			if (norm > tol) {
+				basis.scalarMultEquals(1/norm);
+				basisVectors.add(basis);
+			}
+		}
+
 		basisArrays = new ArrayList<>();
-		int n = basisVectors.get(0).numberOfRows();
 		for (DoubleMatrix dm : basisVectors) {
+			int n = dm.numberOfRows();
 			double[] vals = new double[n];
 			for (int i = 0; i < n; i++) vals[i] = dm.get(i, 0);
 			basisArrays.add(vals);
 		}
 		
 		normalizedData = normalizeVector(allData, true);
-	}
-	
-	private void normalizeData() {
-		
+		modeldf = basisArrays.size() + 2; //plus one for the mean and one for the snp
+		errordf = allData.length - modeldf;
 	}
 	
 	private double[] normalizeVector(double[] input, boolean center) {
@@ -124,8 +164,48 @@ public class DiscreteSitesMatrixFELM extends AbstractFixedEffectLM {
 		
 		double norm = 0;
 		for (int i = 0; i < n; i++) norm += output[i] * output[i];
+		norm = Math.sqrt(norm);
+		if (norm < tol) return new double[0];
 		for (int i = 0; i < n; i++) output[i] /= norm;
 		
 		return output;
 	}
+	
+	private double[] normalizeVector(float[] floatInput, boolean center) {
+		
+		int n = floatInput.length;
+		double[] input = new double[n];
+		if (center) {
+			float mean = 0;
+			for (int i = 0; i < n; i++) mean += floatInput[i];
+			mean /= (double) n;
+			for (int i = 0; i < n; i++) {
+				input[i] = floatInput[i] - mean;
+			}
+		} else {
+			for (int i = 0; i < n; i++) input[i] = floatInput[i];
+		}
+		
+		double[] output = Arrays.copyOf(input, n);
+		
+		for (double[] basis:basisArrays) {
+			double alpha = 0;
+			for (int i = 0; i < n; i++) alpha += input[i]*basis[i];
+			for (int i = 0; i < n; i++) output[i] -= alpha*basis[i];
+		}
+		
+		double norm = 0;
+		for (int i = 0; i < n; i++) norm += output[i] * output[i];
+		norm = Math.sqrt(norm);
+		if (norm < tol) return new double[0];
+		for (int i = 0; i < n; i++) output[i] /= norm;
+		
+		return output;
+	}
+
+	@Override
+	protected void getGenotypeAndUpdateMissing(BitSet missingObsBeforeSite) {
+		missingObsForSite = new OpenBitSet(missingObsBeforeSite);
+	}
+	
 }
