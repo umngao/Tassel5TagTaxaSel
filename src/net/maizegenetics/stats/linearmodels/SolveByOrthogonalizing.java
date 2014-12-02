a777408f8f20a5dc1f97ff0fd6bbf796f854cec3
Peter Bradbury <pjb39@cornell.edu>
2014-12-02 17:36:12 -0500

    TAS-511 Fix bug and add ability to solve add plus dom models to SolveByOrthogonalizing

--- src/net/maizegenetics/stats/linearmodels/SolveByOrthogonalizing.java
+++ src/net/maizegenetics/stats/linearmodels/SolveByOrthogonalizing.java
@@ -14,33 +14,86 @@ import net.maizegenetics.matrixalgebra.decomposition.SingularValueDecomposition;
 
 public class SolveByOrthogonalizing {
 	List<ModelEffect> myBaseModel;
+	List<double[]> myBasisVectors;
 	List<double[]> myData;
 	List<double[]> myOrthogonalizedData;
 	SingularValueDecomposition baseSvd;
 	
-	public SolveByOrthogonalizing(List<ModelEffect> baseModel, List<double[]> dependentVariables) {
-		myBaseModel = baseModel;
-		myData = dependentVariables;
-		computeBaseSvd();
-		OrthogonalizeData();
+	private SolveByOrthogonalizing() {
+		
+	}
+
+	public static SolveByOrthogonalizing getInstanceFromModel(List<ModelEffect> baseModel, List<double[]> dataList) {
+		SolveByOrthogonalizing sbo = new SolveByOrthogonalizing();
+		sbo.myBaseModel = baseModel;
+		sbo.myData = dataList;
+		sbo.computeBaseSvd(sbo.createDesignMatricesFromModel());
+		sbo.OrthogonalizeData();
+		return sbo;
+	}
+	
+	public static SolveByOrthogonalizing getInstanceFromVectors(List<double[]> basisVectors, List<double[]> dataList) {
+		SolveByOrthogonalizing sbo = new SolveByOrthogonalizing();
+		sbo.myBasisVectors = basisVectors;
+		sbo.myData = dataList;
+		sbo.computeBaseSvd(sbo.createDesignMatricesFromVectors());
+		sbo.OrthogonalizeData();
+		return sbo;
 	}
 	
 	public SolveByOrthogonalizing.Marker solveForR(SolveByOrthogonalizing.Marker marker) {
-		return solveForR(marker.position(), marker.values());
+		return solveForR(marker.position(), marker.vector1());
 	}
 	
 	public SolveByOrthogonalizing.Marker solveForR(Position pos, double[] values) {
-		double[] orthogonalValues = centerAndScale(orthogonalizeByBase(values));
+		double[] centeredValues = center(values);
+		double[] orthoValues = orthogonalizeByBase(centeredValues);
+		double[] csVales = centerAndScale(orthoValues);
+		double[] orthogonalValues = centerAndScale(orthogonalizeByBase(center(values)));
 		double[] rValues = myOrthogonalizedData.stream().mapToDouble(d -> innerProduct(d, orthogonalValues)).toArray();
 		return new SolveByOrthogonalizing.Marker(pos, rValues);
 	}
 	
-	private void computeBaseSvd() {
+	public SolveByOrthogonalizing.Marker solveForR(Position pos, double[] add, double[] dom) {
+		if (dom == null) return solveForR(pos, add);
+		double[] orthogonalAdd = orthogonalizeByBase(center(add));
+		double[] orthogonalDom = orthogonalizeByBase(center(dom));
+		
+		//orthogonalize dom with respect to add
+		double mult = innerProduct(orthogonalAdd, orthogonalDom)/innerProduct(orthogonalAdd, orthogonalAdd);
+		int n = orthogonalDom.length;
+		for (int i = 0; i < n; i++) {
+			orthogonalDom[i] -= mult * orthogonalAdd[i];
+		}
+		
+		//center and scale
+		double[] v1 = centerAndScale(orthogonalAdd);
+		double[] v2 = centerAndScale(orthogonalDom);
+		double[] rValues = myOrthogonalizedData.stream()
+				.mapToDouble(d -> innerProduct(d, v1) + innerProduct(d, v2)).toArray();
+		
+		return new SolveByOrthogonalizing.Marker(pos, rValues);
+	}
+	
+	private DoubleMatrix[][] createDesignMatricesFromModel() {
 		DoubleMatrix[][] designMatrices = new DoubleMatrix[1][];
-		designMatrices[0] = (DoubleMatrix[]) myBaseModel.stream()
+		designMatrices[0] = myBaseModel.stream()
 				.filter(a -> !a.getID().toString().toLowerCase().equals("mean"))
 				.map(me -> me.getX())
-				.toArray();
+				.toArray(DoubleMatrix[]::new);
+		return designMatrices;
+	}
+	
+	private DoubleMatrix[][] createDesignMatricesFromVectors() {
+		DoubleMatrix[][] designMatrices = new DoubleMatrix[1][];
+		designMatrices[0] = myBasisVectors.stream()
+				.map(d -> DoubleMatrixFactory.DEFAULT.make(d.length, 1, d))
+				.toArray(DoubleMatrix[]::new);
+		return designMatrices;
+	}
+	
+	private void computeBaseSvd(DoubleMatrix[][] designMatrices ) {
+		
 		
 		DoubleMatrix X = DoubleMatrixFactory.DEFAULT.compose(designMatrices);
 		
@@ -60,20 +113,23 @@ public class SolveByOrthogonalizing {
 	
 	private void OrthogonalizeData() {
 		myOrthogonalizedData = myData.stream()
-				.map(d -> centerAndScale(orthogonalizeByBase(d)))
+				.map(d -> center(d))
+				.map(d -> orthogonalizeByBase(d))
+				.map(d -> centerAndScale(d))
 				.collect(Collectors.toList());
 	}
 	
 	private double[] orthogonalizeByBase(double[] vector) {
 		DoubleMatrix U = baseSvd.getU(false);
 		int nrows = vector.length;
-		double[] start = Arrays.copyOf(vector, nrows);
-		return IntStream.range(0, U.numberOfColumns()).mapToObj(i -> U.column(i).to1DArray())
-			.reduce(start, (a,b) -> {
-				double ip = innerProduct(a, b);
-				for (int j = 0; j < nrows; j++) a[j] = a[j] - b[j] * ip;
-				return a;
-			});
+		double[] result = Arrays.copyOf(vector, nrows);
+		int ncol = U.numberOfColumns();
+		for (int i = 0; i < ncol; i++) {
+			double[] u = U.column(i).to1DArray();
+			double ip = innerProduct(vector, u);
+			for (int j = 0; j < nrows; j++) result[j] -= ip * u[j];
+		}
+		return result;
 	}
 	
 	public static double innerProduct(double[] x, double[] y) {
@@ -96,24 +152,39 @@ public class SolveByOrthogonalizing {
 	
 	public static double[] centerAndScale(double[] values) {
 		int n = values.length;
-		double[] meanDiv = Arrays.stream(values).collect(() -> new double[]{0,0}, (a,b) -> {a[0] =+ b; a[1] += b * b;}, (a,b) -> {a[0] += b[0]; a[1] =+ b[1];});
-		meanDiv[0] /= n;
-		meanDiv[1] = Math.sqrt(meanDiv[1]);
-		for (int i = 0; i < n; i++) values[i] = (values[i] - meanDiv[0]) / meanDiv[1];
+		double sum = 0;
+		double sumsq = 0;
+		for (int i = 0; i < n; i++) {
+			double val = values[i];
+			sum += val;
+			sumsq += val * val;
+		}
+		double mean = sum/n;
+		double divisor = Math.sqrt(sumsq);
+		for (int i = 0; i < n; i++) values[i] = (values[i] - mean) / divisor;
 		return values;
 	}
 	
 	public class Marker {
 		Position myPosition;
-		double[] myValues;
+		double[] vector1;
+		double[] vector2;
 		
 		public Marker(Position pos, double[] values) {
 			myPosition = pos;
-			myValues = values;
+			vector1 = values;
+			vector2 = null;
+		}
+		
+		public Marker(Position pos, double[] additive, double[] dominant) {
+			myPosition = pos;
+			vector1 = additive;
+			vector2 = dominant;
 		}
 		
 		public Position position() { return myPosition; }
-		public double[] values() { return myValues; }
+		public double[] vector1() { return vector1; }
+		public double[] vector2() { return vector2; }
 	}
 	
 	
