e1bb8676c17f1b5137e2d06424f4f0cad209ec06
lynn johnson <lcj34@cornell.edu>
2015-03-09 14:48:13 -0400

    TAS-684: Returning line removed in commit 20c0a5e that added a null annotation in the GeneralPosition constructor.  This line caused junit failures since it was introduced.  THe problem it fixed had been fixed subsequently in a different manner, and all tests I ran succeeded without this line of code.  I have since found code (running pipeline with invariant sites) that fails when the annotation is removed, so I am returning it and will deal with the failed junit in some other way.

--- src/net/maizegenetics/dna/map/GeneralPosition.java
+++ src/net/maizegenetics/dna/map/GeneralPosition.java
@@ -112,6 +112,7 @@ public final class GeneralPosition implements Position {
             myChromosome = Chromosome.getCanonicalChromosome(chr);
             myPosition = position;
             Arrays.fill(myAlleles, GenotypeTable.UNKNOWN_ALLELE);
+            myAnnotations.addAnnotation("VARIANT", "");
         }
 
         /**
