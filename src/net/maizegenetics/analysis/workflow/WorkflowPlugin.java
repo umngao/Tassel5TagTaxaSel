7ee071f55a11e68fbee9591de5c463a17936b8dc
Terry Casstevens <tcasstevens@gmail.com>
2015-03-23 19:36:40 -0400

    TAS-728 Initial Workflow Plugin

---
+++ src/net/maizegenetics/analysis/workflow/WorkflowPlugin.java
@@ -0,0 +1,140 @@
+/*
+ *  WorkflowPlugin
+ * 
+ *  Created on March 20, 2015
+ */
+package net.maizegenetics.analysis.workflow;
+
+import java.net.URL;
+import java.net.URLDecoder;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+import javax.swing.ImageIcon;
+
+import net.maizegenetics.pipeline.TasselPipeline;
+import net.maizegenetics.plugindef.AbstractPlugin;
+import net.maizegenetics.plugindef.DataSet;
+import net.maizegenetics.tassel.TASSELMainFrame;
+import net.maizegenetics.util.Utils;
+
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author Terry Casstevens
+ */
+public class WorkflowPlugin extends AbstractPlugin {
+
+    private static final Logger myLogger = Logger.getLogger(WorkflowPlugin.class);
+
+    private final String myButtonName;
+    private final String[] myArgs;
+    private final TASSELMainFrame myFrame;
+
+    private WorkflowPlugin(TASSELMainFrame parentFrame, String buttonName, String[] args) {
+        super(parentFrame, true);
+        myButtonName = buttonName;
+        myArgs = args;
+        myFrame = parentFrame;
+    }
+
+    public static List<WorkflowPlugin> getInstances(TASSELMainFrame parentFrame) {
+        List<WorkflowPlugin> result = new ArrayList<>();
+        List<String> filenames = getConfigFiles();
+        for (String filename : filenames) {
+            String[] args = new String[]{"-configResourceFile", filename};
+            WorkflowPlugin test = new WorkflowPlugin(parentFrame, buttonName(filename), args);
+            result.add(test);
+        }
+        return result;
+    }
+
+    private static List<String> getConfigFiles() {
+
+        List<String> result = new ArrayList<>();
+
+        try {
+            URL directory = WorkflowPlugin.class.getResource("/net/maizegenetics/analysis/workflow/");
+            String jarPath = directory.getPath().substring(5, directory.getPath().indexOf("!"));
+            JarFile jar = new JarFile(URLDecoder.decode(jarPath, "UTF-8"));
+            Enumeration<JarEntry> entries = jar.entries();
+            while (entries.hasMoreElements()) {
+                String name = entries.nextElement().getName();
+                if ((name.startsWith("net/maizegenetics/analysis/workflow/")) && (name.endsWith(".xml"))) {
+                    result.add("/" + name);
+                }
+            }
+        } catch (Exception e) {
+            myLogger.debug(e.getMessage(), e);
+        }
+
+        return result;
+
+    }
+
+    private static String buttonName(String filename) {
+        String temp = Utils.getFilename(filename, ".xml");
+        StringBuilder builder = new StringBuilder();
+        builder.append(Character.toUpperCase(temp.charAt(0)));
+        boolean newWord = false;
+        for (int i = 1; i < temp.length(); i++) {
+            char current = temp.charAt(i);
+            if ((current == '_') || (current == '-') || (current == ' ')) {
+                builder.append(' ');
+                newWord = true;
+            } else if (Character.isUpperCase(builder.charAt(builder.length() - 1))) {
+                builder.append(current);
+                newWord = false;
+            } else if (Character.isUpperCase(current)) {
+                builder.append(' ');
+                builder.append(current);
+                newWord = false;
+            } else if (newWord) {
+                builder.append(Character.toUpperCase(current));
+                newWord = false;
+            } else {
+                builder.append(current);
+            }
+        }
+        return builder.toString();
+    }
+
+    @Override
+    public DataSet processData(DataSet input) {
+        try {
+            new TasselPipeline(myArgs, myFrame, true);
+            return null;
+        } catch (Exception e) {
+            myLogger.debug(e.getMessage(), e);
+            throw new IllegalStateException("WorkflowPlugin: Problem running workflow: " + myButtonName + "\n" + e.getMessage());
+        } finally {
+            fireProgress(100);
+        }
+    }
+
+    @Override
+    public ImageIcon getIcon() {
+        URL imageURL = WorkflowPlugin.class.getResource("/net/maizegenetics/analysis/images/Workflow.gif");
+        if (imageURL == null) {
+            return null;
+        } else {
+            return new ImageIcon(imageURL);
+        }
+    }
+
+    @Override
+    public String getButtonName() {
+        return myButtonName;
+    }
+
+    @Override
+    public String getToolTipText() {
+        return myButtonName;
+    }
+
+}
