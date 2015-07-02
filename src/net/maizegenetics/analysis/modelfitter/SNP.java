af2f025856c0de54ade4d9237b38027f0bd1c531
Peter Bradbury <pjb39@cornell.edu>
2015-07-02 13:36:14 -0400

    makes all fields in SNP class public

--- src/net/maizegenetics/analysis/modelfitter/SNP.java
+++ src/net/maizegenetics/analysis/modelfitter/SNP.java
@@ -8,8 +8,8 @@ public class SNP {
 	public String name;
 	public Chromosome locus;
 	public int position;
-	ArrayList<Object> alleles;
-	int index;
+	public ArrayList<Object> alleles;
+	public int index;
 	
 	public SNP(String name, Chromosome locus, int positionInLocus, int index) {
 		this.name = name;
