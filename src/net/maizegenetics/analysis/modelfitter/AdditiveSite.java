11e4f3bfdc3e479a14e74d8c571c3a8bc0150c2c
Peter Bradbury <pjb39@cornell.edu>
2015-07-10 14:05:07 -0400

    TAS-832 Cleanup and reformat AdditiveSite and derivatitive classes

--- src/net/maizegenetics/analysis/modelfitter/AdditiveSite.java
+++ src/net/maizegenetics/analysis/modelfitter/AdditiveSite.java
@@ -1,38 +1,42 @@
 package net.maizegenetics.analysis.modelfitter;
 
 public interface AdditiveSite extends Comparable<AdditiveSite> {
-    
-        public static enum CRITERION {
-            ss, 
-            pval, 
-            aic, 
-            bic, 
-            mbic;
-        }
-        
-	/**
-	 * @return		the covariate for this site
-	 */
-	double[] getCovariate();
-	
-	/**
-	 * @param subset	an int array indexing a subset of taxa
-	 * @return			the covariate for the subset of taxa for this sites
-	 */
-	double[] getCovariate(int[] subset);
 
-	/**
-	 * @return	the site index corresponding to the site number in the source GenotypeTable
-	 */
-	int siteNumber();
-	
-	/**
-	 * @return     the value of the selection criterion for this site
-	 */
-	double criterionValue();
-	
-	/**
-	 * @param value        the value of the selection criterion for this site
-	 */
-	void criterionValue(double value);
+    public static enum CRITERION {
+        pval,
+        aic,
+        bic,
+        mbic;
+    }
+
+    /**
+     * @return		the covariate for this site
+     */
+    double[] getCovariate();
+
+    /**
+     * @param subset	an int array indexing a subset of taxa
+     * @return			the covariate for the subset of taxa for this sites
+     */
+    double[] getCovariate(int[] subset);
+
+    /**
+     * @return	the site index corresponding to the site number in the source GenotypeTable
+     */
+    int siteNumber();
+
+    /**
+     * @return     the value of the selection criterion for this site
+     */
+    double criterionValue();
+
+    /**
+     * @param value        the value of the selection criterion for this site
+     */
+    void criterionValue(double value);
+
+    /**
+     * @return     the selection criterion used for this site
+     */
+    CRITERION selectionCriterion();
 }
--- src/net/maizegenetics/analysis/modelfitter/GenotypeAdditiveSite.java
+++ src/net/maizegenetics/analysis/modelfitter/GenotypeAdditiveSite.java
@@ -4,11 +4,6 @@ import net.maizegenetics.dna.snp.GenotypeTable;
 import net.maizegenetics.dna.snp.GenotypeTableUtils;
 
 public class GenotypeAdditiveSite extends AbstractAdditiveSite {
-    private double SumSq;
-    double aic;
-    double bic;
-    double mbic;
-    double pval;
     private int[] bitStore;
     private double[] byteConversion;
     private int ntaxa;
@@ -19,21 +14,23 @@ public class GenotypeAdditiveSite extends AbstractAdditiveSite {
      * @param majorAllele				major allele
      * @param majorAlleleFrequency		major allele frequency
      */
-    public GenotypeAdditiveSite(int site, CRITERION selectionCriterion, byte[] genotype, byte majorAllele, double majorAlleleFrequency) {
-    	super(site, selectionCriterion);
+    public GenotypeAdditiveSite(int site, CRITERION selectionCriterion, byte[] genotype,
+            byte majorAllele, double majorAlleleFrequency) {
+        super(site, selectionCriterion);
         byte unknown = GenotypeTable.UNKNOWN_DIPLOID_ALLELE;
         ntaxa = genotype.length;
         double mean = 2 * majorAlleleFrequency;
-        byteConversion = new double[]{-mean, 1 - mean, 2 - mean, 0};
-        
+        byteConversion = new double[] { -mean, 1 - mean, 2 - mean, 0 };
+
         int numberOfInts = ntaxa / 16;
         int remainder = ntaxa % 16;
-        if (remainder > 0) numberOfInts++;
-        
+        if (remainder > 0)
+            numberOfInts++;
+
         bitStore = new int[numberOfInts];
         int intCount = 0;
         int genoCount = 0;
-        
+
         while (genoCount < ntaxa) {
             int intStore = 0;
             for (int i = 0; i < 32; i += 2) {
@@ -45,21 +42,24 @@ public class GenotypeAdditiveSite extends AbstractAdditiveSite {
                 } else {
                     genoIndex = 0;
                     byte[] alleles = GenotypeTableUtils.getDiploidValues(geno);
-                    if (alleles[0] == majorAllele) genoIndex++;
-                    if (alleles[1] == majorAllele) genoIndex++;
+                    if (alleles[0] == majorAllele)
+                        genoIndex++;
+                    if (alleles[1] == majorAllele)
+                        genoIndex++;
                 }
-                
+
                 intStore = intStore | (genoIndex << i);
-                if (genoCount == ntaxa) break;
+                if (genoCount == ntaxa)
+                    break;
             }
-            
+
             bitStore[intCount++] = intStore;
         }
-        
+
     }
-    
-	@Override
-	public double[] getCovariate() {
+
+    @Override
+    public double[] getCovariate() {
         double[] cov = new double[ntaxa];
         int intCount = 0;
         int genoCount = 0;
@@ -68,14 +68,15 @@ public class GenotypeAdditiveSite extends AbstractAdditiveSite {
             for (int i = 0; i < 32; i += 2) {
                 int genoIndex = (intStore >> i) & 3;
                 cov[genoCount++] = byteConversion[genoIndex];
-                if (genoCount == ntaxa) break;
+                if (genoCount == ntaxa)
+                    break;
             }
         }
         return cov;
-	}
+    }
 
-	@Override
-	public double[] getCovariate(int[] subset) {
+    @Override
+    public double[] getCovariate(int[] subset) {
         int nobs = subset.length;
         double[] cov = new double[nobs];
         int intCount = 0;
@@ -85,10 +86,11 @@ public class GenotypeAdditiveSite extends AbstractAdditiveSite {
             for (int i = 0; i < 32; i += 2) {
                 int genoIndex = (intStore >> i) & 3;
                 cov[genoCount++] = byteConversion[genoIndex];
-                if (genoCount == nobs) break;
+                if (genoCount == nobs)
+                    break;
             }
         }
         return cov;
-	}
+    }
 
 }
