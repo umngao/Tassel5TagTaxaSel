fbc5565a5bf0bd3e74651eb796a23ecba6a61220
Peter Bradbury <pjb39@cornell.edu>
2015-08-24 14:24:25 -0400

    fixes permutation testing in ReferenceProbabilityFELM

--- src/net/maizegenetics/analysis/association/ReferenceProbabilityFELM.java
+++ src/net/maizegenetics/analysis/association/ReferenceProbabilityFELM.java
@@ -12,32 +12,35 @@ import net.maizegenetics.util.BitSet;
 import net.maizegenetics.util.OpenBitSet;
 
 public class ReferenceProbabilityFELM extends AbstractFixedEffectLM {
-	double[] myProbabilities;
+    double[] myProbabilities;
 
-	public ReferenceProbabilityFELM(Datum data, FixedEffectLMPlugin parentPlugin) {
-		super(data, parentPlugin);
-	}
-	
-	@Override
-	protected void analyzeSite() {
-		myModel = new ArrayList<ModelEffect>(myBaseModel);
-		String siteName = myGenoPheno.genotypeTable().siteName(myCurrentSite);
+    public ReferenceProbabilityFELM(Datum data, FixedEffectLMPlugin parentPlugin) {
+        super(data, parentPlugin);
+    }
 
-		myModel.add(new CovariateModelEffect(myProbabilities));
-		
-		if (areTaxaReplicated) myModel.add(taxaEffect());
-		
-		//solve the model
-		SweepFastLinearModel markerModel = new SweepFastLinearModel(myModel, siteData);
+    @Override
+    protected void analyzeSite() {
+        myModel = new ArrayList<ModelEffect>(myBaseModel);
+        String siteName = myGenoPheno.genotypeTable().siteName(myCurrentSite);
+
+        myModel.add(new CovariateModelEffect(myProbabilities));
+
+        if (areTaxaReplicated)
+            myModel.add(taxaEffect());
+
+        //solve the model
+        SweepFastLinearModel markerModel = new SweepFastLinearModel(myModel, siteData);
 
         //calculate model
         double[] modelSSdf = markerModel.getModelcfmSSdf();
         markerSSdf = markerModel.getIncrementalSSdf(numberOfBaseEffects);
-        if (areTaxaReplicated) errorSSdf = markerModel.getIncrementalSSdf(numberOfBaseEffects + 1);
-        else errorSSdf = markerModel.getResidualSSdf();
-        
+        if (areTaxaReplicated)
+            errorSSdf = markerModel.getIncrementalSSdf(numberOfBaseEffects + 1);
+        else
+            errorSSdf = markerModel.getResidualSSdf();
+
         double rsq = markerSSdf[0] / (modelSSdf[0] + markerModel.getResidualSSdf()[0]);
-        
+
         double F = markerSSdf[0] / markerSSdf[1] / errorSSdf[0] * errorSSdf[1];
         double p;
         try {
@@ -46,20 +49,22 @@ public class ReferenceProbabilityFELM extends AbstractFixedEffectLM {
             p = Double.NaN;
         }
         double[] beta = markerModel.getBeta();
-		if (permute) G = markerModel.getInverseOfXtX();
-		
+        if (permute)
+            G = markerModel.getInverseOfXtX();
+
         //add results to site report
         //{"Trait","Marker","Chr","Pos","marker_F","p","marker_Rsq","marker_df","marker_MS","error_df","error_MS","model_df","model_MS" }
         if (maxP == 1.0 || p <= maxP) {
-    		Object[] rowData = new Object[numberOfSiteReportColumns];
+            Object[] rowData = new Object[numberOfSiteReportColumns];
             int columnCount = 0;
             rowData[columnCount++] = currentTraitName;
-            rowData[columnCount++] = siteName;	
+            rowData[columnCount++] = siteName;
             rowData[columnCount++] = myGenoPheno.genotypeTable().chromosomeName(myCurrentSite);
             rowData[columnCount++] = myGenoPheno.genotypeTable().chromosomalPosition(myCurrentSite);
             rowData[columnCount++] = new Double(F);
             rowData[columnCount++] = new Double(p);
-            if (permute) rowData[columnCount++] = "";
+            if (permute)
+                rowData[columnCount++] = "";
             rowData[columnCount++] = new Double(rsq);
             rowData[columnCount++] = new Double(markerSSdf[1]);
             rowData[columnCount++] = new Double(markerSSdf[0] / markerSSdf[1]);
@@ -68,7 +73,9 @@ public class ReferenceProbabilityFELM extends AbstractFixedEffectLM {
             rowData[columnCount++] = new Double(modelSSdf[1]);
             rowData[columnCount++] = new Double(modelSSdf[0] / modelSSdf[1]);
             siteReportBuilder.add(rowData);
-            
+            if (permute)
+                siteTableReportRows.add(rowData);
+
             //add results to allele report
             //{"Trait","Marker","Chr","Position","Estimate"}
             int estimateIndex = beta.length - 1;
@@ -81,36 +88,48 @@ public class ReferenceProbabilityFELM extends AbstractFixedEffectLM {
             rowData[columnCount++] = beta[estimateIndex];
             alleleReportBuilder.add(rowData);
         }
-	}
+    }
 
-	@Override
-	protected void getGenotypeAndUpdateMissing(BitSet missingObsBeforeSite) {
-		float[] allSiteProbs = myGenoPheno.referenceProb(myCurrentSite);
-		
-		int n = allSiteProbs.length;
-		missingObsForSite = new OpenBitSet(missingObsBeforeSite);
-		for (int i = 0; i < n; i++) {
-			if (Float.isNaN(allSiteProbs[i])) missingObsForSite.fastSet(i);
-		}
-		myProbabilities = AssociationUtils.getNonMissingDoubles(allSiteProbs, missingObsForSite);
-	}
+    @Override
+    protected void getGenotypeAndUpdateMissing(BitSet missingObsBeforeSite) {
+        float[] allSiteProbs = myGenoPheno.referenceProb(myCurrentSite);
 
-	@Override
-	protected String[] siteReportColumnNames() {
-		if (permute) return new String[]{AssociationConstants.STATS_HEADER_TRAIT,AssociationConstants.STATS_HEADER_MARKER,
-		        AssociationConstants.STATS_HEADER_CHR,AssociationConstants.STATS_HEADER_POSITION,"marker_F",AssociationConstants.STATS_HEADER_P_VALUE,
-		        "perm_p","marker_Rsq","marker_df","marker_MS","error_df","error_MS","model_df","model_MS" };
-		return new String[] {AssociationConstants.STATS_HEADER_TRAIT,AssociationConstants.STATS_HEADER_MARKER,AssociationConstants.STATS_HEADER_CHR,
-		        AssociationConstants.STATS_HEADER_POSITION,"marker_F",AssociationConstants.STATS_HEADER_P_VALUE,"marker_Rsq","marker_df","marker_MS",
-		        "error_df","error_MS","model_df","model_MS" };
-	
-	}
+        int n = allSiteProbs.length;
+        missingObsForSite = new OpenBitSet(missingObsBeforeSite);
+        for (int i = 0; i < n; i++) {
+            if (Float.isNaN(allSiteProbs[i]))
+                missingObsForSite.fastSet(i);
+        }
+        myProbabilities = AssociationUtils.getNonMissingDoubles(allSiteProbs, missingObsForSite);
+    }
 
-	@Override
-	protected String[] alleleReportColumnNames() {
-		// TODO Auto-generated method stub
-		return new String[]{AssociationConstants.STATS_HEADER_TRAIT,AssociationConstants.STATS_HEADER_MARKER,
-                        AssociationConstants.STATS_HEADER_CHR,AssociationConstants.STATS_HEADER_POSITION,"Estimate"};
-	}
+    @Override
+    protected String[] siteReportColumnNames() {
+        markerpvalueColumn = 5;
+        permpvalueColumn = 6;
+        if (permute)
+            return new String[] { AssociationConstants.STATS_HEADER_TRAIT,
+                    AssociationConstants.STATS_HEADER_MARKER,
+                    AssociationConstants.STATS_HEADER_CHR,
+                    AssociationConstants.STATS_HEADER_POSITION, "marker_F",
+                    AssociationConstants.STATS_HEADER_P_VALUE,
+                    "perm_p", "marker_Rsq", "marker_df", "marker_MS", "error_df", "error_MS",
+                    "model_df", "model_MS" };
+        return new String[] { AssociationConstants.STATS_HEADER_TRAIT,
+                AssociationConstants.STATS_HEADER_MARKER, AssociationConstants.STATS_HEADER_CHR,
+                AssociationConstants.STATS_HEADER_POSITION, "marker_F",
+                AssociationConstants.STATS_HEADER_P_VALUE, "marker_Rsq", "marker_df", "marker_MS",
+                "error_df", "error_MS", "model_df", "model_MS" };
+
+    }
+
+    @Override
+    protected String[] alleleReportColumnNames() {
+        // TODO Auto-generated method stub
+        return new String[] { AssociationConstants.STATS_HEADER_TRAIT,
+                AssociationConstants.STATS_HEADER_MARKER,
+                AssociationConstants.STATS_HEADER_CHR, AssociationConstants.STATS_HEADER_POSITION,
+                "Estimate" };
+    }
 
 }
