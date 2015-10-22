bcce377ec6d37ee79c0435cb7fc5332f63616764
Terry Casstevens <tcasstevens@gmail.com>
2015-10-21 23:19:48 -0400

    Initial implementation of MaskGenotypeCallTable

---
+++ src/net/maizegenetics/dna/snp/MaskGenotypeTableBuilder.java
@@ -0,0 +1,46 @@
+/*
+ *  MaskGenotypeTableBuilder
+ * 
+ *  Created on Oct 21, 2015
+ */
+package net.maizegenetics.dna.snp;
+
+import net.maizegenetics.util.BitSet;
+import net.maizegenetics.util.OpenBitSet;
+
+/**
+ *
+ * @author Terry Casstevens
+ */
+public class MaskGenotypeTableBuilder {
+    
+    private final byte DEFAULT_MASK_VALUE = GenotypeTable.UNKNOWN_DIPLOID_ALLELE;
+
+    private final GenotypeTable myBase;
+    private final BitSet[] myBitSets;
+    private byte myMaskValue = DEFAULT_MASK_VALUE;
+
+    public MaskGenotypeTableBuilder(GenotypeTable base) {
+        myBase = base;
+        int numSites = myBase.numberOfSites();
+        int numTaxa = myBase.numberOfTaxa();
+        myBitSets = new BitSet[numTaxa];
+        for (int t = 0; t < numTaxa; t++) {
+            myBitSets[t] = new OpenBitSet(numSites);
+        }
+    }
+    
+    public void setMaskValue (byte value) {
+        myMaskValue = value;
+    }
+    
+    public void set (int taxon, int site) {
+        myBitSets[taxon].fastSet(site);
+    }
+    
+    public GenotypeTable build() {
+        //return new CoreGenotypeTable(new MaskGenotypeCalTable(), null, null, null, null, null, null, null);
+        throw new UnsupportedOperationException();
+    }
+
+}
