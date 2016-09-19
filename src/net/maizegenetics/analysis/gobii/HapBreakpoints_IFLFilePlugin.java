78a8e86408dd03d797a0571d3a634ec8b32d42de
Lynn Johnson <lcjdenver@gmail.com>
2016-09-19 13:02:18 -0400

    TAS-1098 Updates to HapBreakpoints_IFLFIlePlugin
    THis plugin creates intermediate files for loading into GOBII

--- src/net/maizegenetics/analysis/gobii/HapBreakpoints_IFLFilePlugin.java
+++ src/net/maizegenetics/analysis/gobii/HapBreakpoints_IFLFilePlugin.java
@@ -42,7 +42,7 @@ import net.maizegenetics.util.Utils;
  * hapbreakpoint Table:
  *   hap_breakpoint_id  int
  *   taxa(GID)          int 
- *   positionrange      int4range (start/stop stored as an integer range)
+ *   position_range      int4range (start/stop stored as an integer range)
  *   donor1 (GID)       int
  *   donor2 (GID)       int
  *   breakpoint_set_id  int  (maps to breapoint_set table)
@@ -79,6 +79,11 @@ import net.maizegenetics.util.Utils;
  * which is named ZeaWGS_hmp321_raw_AGPv3.  The taxa mapping file is the file Cinta
  * provides with each subset, e.g. for Ames in Cornell box.
  * 
+ * If the TaxaColumn in the mapping file contains a libraryID (e.g. name:libraryID)
+ * the code removes it, leaving taxa set to just the "name" portion.  THis is because
+ * Kelly's pa.txt.gz files do NOT have the library portion, though some of Cinta's
+ * files do.
+ * 
  * The breakpoint file has donors at top:  Donors are Feb-48,PHG84, PHG83, etc.
  * Their GIDs come from the WGS mapping file Cinta gave for curating that large vcf set.
  * 1210 2711
@@ -98,6 +103,11 @@ import net.maizegenetics.util.Utils;
  * The third table, the projection_alignment table, will be populated with
  * another plugin.  Users will create their own projection alignment analyses
  * for the breakpoint sets of their choice.
+ * 
+ * -------------
+ * Sept 14, 2016:  Trying with Cinta's "name2" field, as the taxa are not
+ * matching from the taxa column for the taxa mapping file.  Donors are working.
+ * 
  * @author lcj34
  *
  */
@@ -144,6 +154,7 @@ public class HapBreakpoints_IFLFilePlugin extends AbstractPlugin {
         }
  
         if (dirList.isDirectory()) {
+            System.out.println("LCJ - breakfile is a directory, creating list of files ...");
             String inputFileGlob="glob:*{pa.txt,pa.txt.gz}";
             infiles= DirectoryCrawler.listPaths(inputFileGlob, Paths.get(breakFile.value()).toAbsolutePath());
             if (infiles.size() < 1) {
@@ -155,9 +166,17 @@ public class HapBreakpoints_IFLFilePlugin extends AbstractPlugin {
             infiles.add(filePath);
         }
         Collections.sort(infiles); 
+        System.out.println("LCJ - size of infiles: " + infiles.size());
     } 
     //This takes the mapping file, returns a hashmap of taxaname/GID - both strings
-    public HashMap<String,String> createNameGidMap(String mappingFile) {
+    // For now, I will strip off anything from a colon and beyond in the TaxaColumn.
+    // Cinta added both taxa and libraryID, thinking that is what the vcf/hmp file
+    // would have.  Some do.  Others don't.  Kelly's PA files do NOT have the library ID
+    // appended, so no taxa is found.  If there is much variation between files,
+    // then whether to expect the libraryID will need to be a parameter to the plugin.
+    
+    
+    public HashMap<String,String> createNameGidMap(String mappingFile, String taxaField) {
         HashMap<String,String> taxaMapArray = new HashMap<String,String>(); 
         BufferedReader taxaMapFilebr = Utils.getBufferedReader(mappingFile, 1 << 22);
         String taxaMapLine;
@@ -169,7 +188,7 @@ public class HapBreakpoints_IFLFilePlugin extends AbstractPlugin {
                 String [] headers = taxaMapLine.split("\\t");
                 int idx = 0;
                 for (String header : headers) {
-                    if (header.trim().toUpperCase().equals("TAXACOLUMN")) {
+                    if (header.trim().toUpperCase().equals(taxaField)) {
                         taxaIdx = idx;
                     } else if (header.trim().toUpperCase().equals("GID")) {
                         gidIdx = idx;
@@ -186,9 +205,11 @@ public class HapBreakpoints_IFLFilePlugin extends AbstractPlugin {
                 // this is the mapping file - read and pull taxaname/gid, store in map
                 String[] data = taxaMapLine.split("\\t");
                 String taxaname = data[taxaIdx].trim();
-                if (taxaname.contains("030-1")) {
-                    System.out.println("LCJ - createNameGIdMap - found taxaname 030-1");
+                int colonIndex = taxaname.indexOf(":");
+                if (colonIndex > 0) { // remove libraryId if it was appended
+                    taxaname = taxaname.substring(0, colonIndex);
                 }
+
                 String gid = data[gidIdx].trim();
                 if (printTaxa)
                    System.out.println("   creatNameGIDMap, adding taxa:" + taxaname + " with gid " + gid);
@@ -227,8 +248,9 @@ public class HapBreakpoints_IFLFilePlugin extends AbstractPlugin {
             
             // Read the taxa mapping file, create array of names/maps
             System.out.println("LCJ - calling createNameGidMap for taxaMapFile");
-            HashMap<String,String> taxaNameGid = createNameGidMap(taxaMapFile());
-            if (taxaNameGid == null) {
+            HashMap<String,String> taxaNameGidMap = createNameGidMap(taxaMapFile(),"NAME");
+            if (taxaNameGidMap == null) {
+            //if (taxaNameGidMap != null) { // RETURN CODE ABOVE - this is just for debugging, printing names
                 System.out.println("ERROR - could not create taxaname-gid mapping");
                 return null;
             }
@@ -236,16 +258,18 @@ public class HapBreakpoints_IFLFilePlugin extends AbstractPlugin {
             //printTaxa = true;
             
             System.out.println("\nLCJ - calling createNameGIDMap for donorMapFile");
-            HashMap<String,String> donorNameGid = createNameGidMap(donorMapFile());
-            if (donorNameGid == null) {
+            HashMap<String,String> donorNameGidMap = createNameGidMap(donorMapFile(),"TAXACOLUMN");
+            if (donorNameGidMap == null) {
                 System.out.println("ERROR - could not create taxaname-gid mapping");
                 return null;
             }
             System.out.println("HapBreakpoints:processData begin\n");
-            String hapbrkFile = outputDir() + setName() + ".hap_breakpoint"; 
+            String hapbrkFile = outputDir() + setName() + ".hapbreakpoints"; 
             String breakSetFile = outputDir() +  setName() + ".breakpoint_set";
             BufferedWriter writerhap = Utils.getBufferedWriter(hapbrkFile);
             BufferedWriter writerbrkset = Utils.getBufferedWriter(breakSetFile);
+            BufferedWriter taxaNotFoundbw = Utils.getBufferedWriter("/Users/lcj34/notes_files/gobiiANDBms/gobii_loading/gobii_ifl_files/gobii_hapbreakpoints/taxaNotFound.txt");
+            BufferedWriter taxaFoundbw = Utils.getBufferedWriter("/Users/lcj34/notes_files/gobiiANDBms/gobii_loading/gobii_ifl_files/gobii_hapbreakpoints/taxaFound.txt");
             
             
             // Create header lines for each intermediate file
@@ -256,25 +280,27 @@ public class HapBreakpoints_IFLFilePlugin extends AbstractPlugin {
             // write header lines: NOTE: breakpoint_set_name is for IFL to grab breakpoint_set_id value
             // this needs to appear in a hap_breakpoint.nmap file
             // also need to define the duplicate files
-            hapSB.append("name\ttaxa\tpositionRange\tdonor1\tdonor2\tbreakpoint_set_name\n");
+            hapSB.append("taxa_gid\tchr\tposition_range\tdonor1\tdonor2\tstatus\tbreakpoint_set_name\n");
             writerhap.write(hapSB.toString());
             hapSB.setLength(0);
             
             // breakpoint_set table intermediate file. NOTE: mapset_name should be used by IFL to grab mapset_id
             // this means we don't really need to connect to DB - let IFL do it.
             // This also means don't need dbconfig parameter.  Remove it when verify is good without.
-            brksetSB.append("name\tmethod\tmapset_name\n"); // header line
+            brksetSB.append("name\tmethod\tmapset_name\tstatus\n"); // header line
             brksetSB.append(setName()); // begin data line
             brksetSB.append("\t");
             brksetSB.append(method());
             brksetSB.append("\t");
             brksetSB.append(mapset());
-            brksetSB.append("\n");           
+            brksetSB.append("\t1\n"); // status and newline          
             writerbrkset.write(brksetSB.toString()); // file has header and just this one line
             writerbrkset.close();
             
             BufferedReader breakerbr = null;
             String[] donorList = null;
+            int totalTaxaFound = 0;
+            int totalTaxaNOTFound = 0;
             System.out.println("processData: number of infiles: " + infiles.size());
             for (Path filepath: infiles) {
                 String curFile = filepath.toString();
@@ -295,23 +321,27 @@ public class HapBreakpoints_IFLFilePlugin extends AbstractPlugin {
                 System.out.println(" ... creatingDonorLists ..., number of donors is " + numDonors);
                 donorList = new String[numDonors];
                 boolean allFound = true;
-                for (int idx = 0;idx < numDonors;idx++){
-                    String donorLine = breakerbr.readLine();
-                    if (donorLine.startsWith("#")) continue; // skip comment line
-                    String[] donorInfo = donorLine.split("\\t");
-                    String donorGid = donorNameGid.get(donorInfo[1].trim());
+                int donorCount = 0;
+                for (int idx = 0;donorCount < numDonors;idx++){
+                    mline = breakerbr.readLine();
+                    if (mline.startsWith("#")) continue; // skip comment line
+                    String[] donorInfo = mline.split("\\t");
+                    String donorGid = donorNameGidMap.get(donorInfo[1].trim());
                     if (donorGid == null) {
                         System.out.println("LCJ - no donorGId found in donorMap for donor " + donorInfo[1]);
+                        donorList[donorCount] = "49999999"; // dummy to get things working
                         allFound = false;
                     } else {
-                        donorList[idx] = donorGid;
+                        donorList[donorCount] = donorGid;
                     }
-                    donorList[idx] = donorGid;                 
+                    donorCount++;
+                 
                 }
                 if (!allFound) return null;
 
                 System.out.println("Processing the breakpoint file ...");
                 // Continue processing breakpoint file.  We processed all the donors, now process each block line
+                
                 while ( (mline=breakerbr.readLine() )!= null) {
                     hapSB.setLength(0); // reset line to 0
                     if (mline.startsWith("#")) continue;  // skip comment lines
@@ -321,15 +351,22 @@ public class HapBreakpoints_IFLFilePlugin extends AbstractPlugin {
                     String tokens[] = mline.split("\t");
                     String gidForTaxa = "-1";
                     String taxa = tokens[0].trim();
-                    if (taxaNameGid.containsKey(taxa)) {
-                        gidForTaxa = taxaNameGid.get(taxa);
+                    if (taxaNameGidMap.containsKey(taxa)) {
+                        gidForTaxa = taxaNameGidMap.get(taxa);
                     }
                     if (gidForTaxa.equals("-1")) {
-                        System.out.println("ERROR: gidForTaxa map does not contain taxa " + taxa + " from block list: " + mline);
+                        totalTaxaNOTFound++;
+                        taxaNotFoundbw.write(taxa);
+                        taxaNotFoundbw.write("\n");
+                       // System.out.println("ERROR: gidForTaxa map does not contain taxa " + taxa + " from block list: " + mline);
                         //return null; // LCJ - want to return this error - for now I want to see if we can process anything
-                        continue;
+                        gidForTaxa = "49999999"; // add a dummy for now - FIX THIS WITH REAL TAXA
+                        //continue; keep going - I want to see what I can process
                     }
-                    System.out.println("SUCCESS: found taxa " + taxa );
+                    totalTaxaFound++;
+                    taxaFoundbw.write(taxa);
+                    taxaFoundbw.write("\n");
+                    //System.out.println("SUCCESS: found taxa " + taxa );
                     // Process blocks within each line
                     // Starting blockIdx at 1 so we pass over the taxa in 0, which was already processed
                     for (int blockIdx = 1;blockIdx < tokens.length; blockIdx++) {
@@ -357,7 +394,7 @@ public class HapBreakpoints_IFLFilePlugin extends AbstractPlugin {
                             System.out.println("LCJ - donor2 in block is out of range: " + block);
                             hapSB.append("-1"); // will be obvious is isn't a GID, which start at 42000000
                         }
-                        hapSB.append("\t");
+                        hapSB.append("\t1\t"); // adding tabs and default status code
                         hapSB.append(setName()); // from input parameter
                         hapSB.append("\n");                                                                    
                     } 
@@ -367,6 +404,10 @@ public class HapBreakpoints_IFLFilePlugin extends AbstractPlugin {
             }
             if (breakerbr != null) breakerbr.close(); 
             writerhap.close(); // writerbrkset closed above
+            taxaFoundbw.close();
+            taxaNotFoundbw.close();
+            System.out.println("LCJ - end of plugin, total taxa found within blocks: " + totalTaxaFound
+                    + " total taxa NOT found: " + totalTaxaNOTFound);
         } catch (Exception exc) {
             System.out.println("processData: caught exception processing or writing files");
             exc.printStackTrace();
