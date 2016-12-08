acd9bce5f48c28e4425299b1e6bf76e223b303cf
Lynn Johnson <lcjdenver@gmail.com>
2016-12-08 13:57:29 -0500

    TAS-1152 Deleted commented-out code.

--- src/net/maizegenetics/dna/tag/RepGenSQLite.java
+++ src/net/maizegenetics/dna/tag/RepGenSQLite.java
@@ -71,10 +71,11 @@ public class RepGenSQLite implements RepGenDataWriter, AutoCloseable {
         of these objects over and over again.
      */
     private BiMap<Tag,Integer> tagTagIDMap; // Tag is AbstractTag.java
-    // This gets tricky.  The reference tag sequence can show up in different places.
-    // refTag is unique based on sequence, chrom, position.  Sequence can be duplicated
-    // on same chrom and on multiple chroms.  How to get this into a tagId map ??
-    // Should this also indicate the referenceGenome used ??
+    
+    // RefTagData is the key to BiMap refTagRefTagIDMAP because:
+    // The reference tag sequence can show up in different places.
+    // refTag is unique based on sequence, chromosome, seqlen, position, refGenomeID.  
+    // Sequence can be duplicated on same chrom and on multiple chroms.
     private BiMap<RefTagData,Integer> reftagReftagIDMap; // Tag is AbstractRefTag.java
     private Map<String,Integer> mappingApproachToIDMap;
     private Map<String,Integer> referenceGenomeToIDMap;
@@ -1659,61 +1660,3 @@ public class RepGenSQLite implements RepGenDataWriter, AutoCloseable {
         return null;
     }
 }
-
-//class RefTagData {
-//    private final Tag myTag;
-//    private final String chromosome;
-//    private final int position;
-//    private final int refGenomeID;
-//
-//    public RefTagData(Tag myTag, String chromosome,int position, int refGenomeID) {
-//        this.myTag = myTag;
-//        this.chromosome = chromosome;
-//        this.position = position;
-//        this.refGenomeID = refGenomeID;
-//    }
-//
-//    public Tag tag() {
-//        return myTag;
-//    }
-//
-//    public String chromosome() {
-//        return chromosome;
-//    }
-//    public int position() {
-//        return position;
-//    }
-//    public int refGenomeID() {
-//        return refGenomeID;
-//    }
-//    
-//    @Override
-//    public boolean equals(Object obj) {
-//        if (this == obj) return true;
-//        if (obj == null || getClass() != obj.getClass()) return false;
-//
-//        RefTagData that = (RefTagData) obj;
-//
-//        if (!(tag().equals(that.tag()))) return false;
-//        if (!(chromosome().equals(that.chromosome()))) return false;
-//        if (position() != that.position()) return false;
-//        if (refGenomeID() != that.refGenomeID()) return false;
-//
-//        return true;
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        int hash = 7;
-//        hash = 37 * hash + this.myTag.hashCode();
-//        try {
-//            hash = 37 * hash + Integer.parseInt(chromosome);
-//        } catch (NumberFormatException nfe) {
-//            // add nothing for chromsome if not int
-//        }
-//        hash = 37 * hash + this.position;
-//        hash = 37 * hash + this.refGenomeID;
-//        return hash;
-//    }
-//
-//}
