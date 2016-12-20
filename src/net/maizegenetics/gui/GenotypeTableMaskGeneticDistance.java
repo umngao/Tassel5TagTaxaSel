7c9f0530c66f8ecf1695cd36d1034a9a6a3b5b8f
Terry Casstevens <tcasstevens@gmail.com>
2016-12-19 23:53:59 -0500

    Improved Genetic Distance heat map to sort taxa based genetic distances

--- src/net/maizegenetics/gui/GenotypeTableMaskGeneticDistance.java
+++ src/net/maizegenetics/gui/GenotypeTableMaskGeneticDistance.java
@@ -3,14 +3,9 @@
  */
 package net.maizegenetics.gui;
 
-import net.maizegenetics.analysis.distance.IBSDistanceMatrix;
-import net.maizegenetics.taxa.Taxon;
-import net.maizegenetics.dna.snp.GenotypeTable;
-
 import java.awt.*;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
+import net.maizegenetics.dna.snp.GenotypeTable;
+import net.maizegenetics.taxa.Taxon;
 
 /**
  *
@@ -18,56 +13,43 @@ import java.util.Map;
  */
 public class GenotypeTableMaskGeneticDistance extends AbstractGenotypeTableMask {
 
-    private static final long serialVersionUID = -5197800047652332969L;
-    private Map<Integer, Byte> myCache = new LinkedHashMap<Integer, Byte>() {
-        protected boolean removeEldestEntry(Map.Entry<Integer, Byte> eldest) {
-            return size() > 100;
-        }
-    };
     private final int myTaxonReference;
     private final GenotypeTable myAlignment;
+    private final double[] myDistances;
 
-    private GenotypeTableMaskGeneticDistance(GenotypeTable align, int taxonReference, String name, Color color) {
+    private GenotypeTableMaskGeneticDistance(GenotypeTable align, int taxonReference, double[] distances, String name, Color color) {
         super(align, name, color, GenotypeTableMask.MaskType.reference);
         myTaxonReference = taxonReference;
         myAlignment = align;
+        myDistances = distances;
     }
 
-    public static GenotypeTableMaskGeneticDistance getInstanceCompareReference(GenotypeTable align, Taxon id) {
+    public static GenotypeTableMaskGeneticDistance getInstanceCompareReference(GenotypeTable align, Taxon id, double[] distances) {
         int index = align.taxa().indexOf(id);
         if (index < 0) {
             throw new IllegalArgumentException("GenotypeTableMaskGeneticDistance: getInstanceCompareReference: unknown id: " + id);
         }
-        return getInstanceCompareReference(align, index);
+        return getInstanceCompareReference(align, index, distances);
     }
 
-    public static GenotypeTableMaskGeneticDistance getInstanceCompareReference(GenotypeTable align, String id) {
+    public static GenotypeTableMaskGeneticDistance getInstanceCompareReference(GenotypeTable align, String id, double[] distances) {
         int index = align.taxa().indexOf(id);
         if (index < 0) {
             throw new IllegalArgumentException("GenotypeTableMaskGeneticDistance: getInstanceCompareReference: unknown id: " + id);
         }
-        return getInstanceCompareReference(align, index);
+        return getInstanceCompareReference(align, index, distances);
     }
 
-    public static GenotypeTableMaskGeneticDistance getInstanceCompareReference(GenotypeTable align, int index) {
+    public static GenotypeTableMaskGeneticDistance getInstanceCompareReference(GenotypeTable align, int index, double[] distances) {
         if ((index < 0) || (index >= align.numberOfTaxa())) {
             throw new IllegalArgumentException("GenotypeTableMaskGeneticDistance: getInstanceCompareReference: unknown index: " + index);
         }
         String name = align.taxaName(index) + " Genetic Distance";
-        return new GenotypeTableMaskGeneticDistance(align, index, name, null);
+        return new GenotypeTableMaskGeneticDistance(align, index, distances, name, null);
     }
 
     @Override
     public byte getMask(int taxon, int site) {
-
-        Byte result = myCache.get(taxon);
-        if (result != null) {
-            return result;
-        }
-
-        result = (byte) (IBSDistanceMatrix.computeHetBitDistances(myAlignment, taxon, myTaxonReference)[0] * 255.0);
-        myCache.put(taxon, result);
-        return result;
-
+        return (byte) (myDistances[taxon] * 255.0);
     }
 }
