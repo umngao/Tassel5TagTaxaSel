ac22efe9d087d98bf3bb27f246c3319d7ce06fbd
liang_think540 <lgao@umn.edu>
2017-07-27 18:10:39 -0500

    update source to allow input sel tags file

--- src/net/maizegenetics/analysis/gbs/v2/GetTagTaxaDistFromDBPlugin.java
+++ src/net/maizegenetics/analysis/gbs/v2/GetTagTaxaDistFromDBPlugin.java
@@ -4,14 +4,20 @@
 package net.maizegenetics.analysis.gbs.v2;
 
 import java.awt.Frame;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Set;
+import java.util.StringTokenizer;
+import java.io.FileReader;
+import java.io.File;
 
 import javax.swing.ImageIcon;
 
 import org.apache.log4j.Logger;
+
+// import com.sleepycat.je.log.FileReader;
 
 import net.maizegenetics.dna.tag.Tag;
 import net.maizegenetics.dna.tag.TagDataSQLite;
@@ -35,11 +41,11 @@ public class GetTagTaxaDistFromDBPlugin extends AbstractPlugin {
 
     private PluginParameter<String> myDBFile = new PluginParameter.Builder<String>("db", null, String.class).guiName("Input DB").required(true).inFile()
             .description("Input database file with tags").build();
-    private PluginParameter<String> myTagFile = new PluginParameter.Builder<String>("seltags", null, String.class).guiName("Seltags File").required(true).inFile()
-            .description("Input selected tags file with tags id in 2nd column").build();
+    private PluginParameter<String> myTagFile = new PluginParameter.Builder<String>("tg", null, String.class).guiName("Input Tag File").required(true).inFile()
+    		.description("Input tag file with prefered tags").build();
     private PluginParameter<String> myOutputFile = new PluginParameter.Builder<String>("o", null, String.class).guiName("Output File").required(true).outFile()
             .description("Output txt file that can be imported to Excel").build();
-    
+
     public GetTagTaxaDistFromDBPlugin() {
         super(null, false);
     }
@@ -56,6 +62,8 @@ public class GetTagTaxaDistFromDBPlugin extends AbstractPlugin {
     @Override
     public DataSet processData(DataSet input) {
         TagDataWriter tdw = new TagDataSQLite(inputDB());
+        // Create a reader to read the TagFile
+        BufferedReader tagFileReader = null;
         try {
             BufferedWriter fileWriter = null;
             StringBuilder strB = new StringBuilder();
@@ -66,9 +74,49 @@ public class GetTagTaxaDistFromDBPlugin extends AbstractPlugin {
                 strB.append(item.getName());
             });
             strB.append("\n");
-            int mySelTags[] = {1,3,5,7};
-            Set<Tag> myTags = tdw.getTags();
-            		
+            // Start to read the selected tagID from the tagFile
+            tagFileReader = new BufferedReader(new FileReader(new File(inputTagFile())));
+            String tempString = null;
+        	int srcline = 0;
+        	int i = 0;
+        	// Read the header of the tagFile
+        	tagFileReader.readLine();
+        	// Read each line to get the total tag number
+        	while ((tempString = tagFileReader.readLine()) != null) {
+        		StringTokenizer token=new StringTokenizer(tempString,"\t");
+        		String[] array=new String[token.countTokens()];
+        		i=0;
+        		while(token.hasMoreTokens()){
+        			array[i]=token.nextToken();
+        			i++;
+        		}
+        		if (Integer.parseInt(array[1])>=1)
+        			srcline++;
+        	}
+        	// System.out.println("Total tags: "+srcline);
+        	tagFileReader.close();
+        	// Initialize mySelTags
+        	int mySelTags[] = new int[srcline];
+        	// Get each Tag ID and generate mySelTags array
+        	tagFileReader = new BufferedReader(new FileReader(new File(inputTagFile())));
+        	// Read the header of the tagFile
+        	tagFileReader.readLine();
+        	while ((tempString = tagFileReader.readLine()) != null) {
+        		StringTokenizer token=new StringTokenizer(tempString,"\t");
+        		String[] array=new String[token.countTokens()];
+        		i=0;
+        		while(token.hasMoreTokens()){
+        			array[i]=token.nextToken();
+        			i++;
+        		}
+        		if (Integer.parseInt(array[1])>=1) {
+        			mySelTags[srcline] = Integer.parseInt(array[1]);
+        			srcline++;
+        		}
+        	}
+        	tagFileReader.close();
+            // End of generate the mySelTags array
+            Set<Tag> myTags = tdw.getTags();            		
             int tagcount = 0;
             for (Tag myTag: myTags) {
                 //System.out.println(myTag.toString());
@@ -154,7 +202,15 @@ public class GetTagTaxaDistFromDBPlugin extends AbstractPlugin {
         myDBFile = new PluginParameter<>(myDBFile, value);
         return this;
     }
-
+    /**
+     * Input preferred tag file with tags and taxa distribution
+     *
+     * @return Input preffered tag file
+     */
+    public String inputTagFile() {
+        return myTagFile.value();
+    }
+    
     /**
      * Output tab-delimited file showing tag sequences found in DB.
      *
